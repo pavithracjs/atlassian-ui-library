0bb7f4d0e9c46f4dc7b9d4ddd0c9821ae0abde52
Jake Coppinger <jcoppinger@atlassian.com>
2018-12-11 16:25:20 +1100

    Delete dark editor example in editor-core, changed vr test to use dark full page with content

--- packages/editor/editor-core/src/__tests__/visual-regression/mobile-dark-editor.ts
+++ packages/editor/editor-core/src/__tests__/visual-regression/mobile-dark-editor.ts
@@ -5,7 +5,7 @@ describe('Snapshot Test: Mobile Dark Editor', () => {
   beforeAll(async () => {
     // @ts-ignore
     page = global.page;
-    await initEditor(page, 'dark-mobile');
+    await initEditor(page, 'dark-full-page-with-content');
   });
 
   it('should correctly render dark mode in mobile editor', async () => {
