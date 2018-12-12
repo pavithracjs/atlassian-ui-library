7561b9f52b3c77e6e97850e5b0250a91b7069118
Jake Coppinger <jcoppinger@atlassian.com>
2018-12-12 15:29:01 +1100

    Testing light and dark colours in date

--- packages/elements/date/src/__tests__/components/unit/DateLozengeSpec.tsx
+++ packages/elements/date/src/__tests__/components/unit/DateLozengeSpec.tsx
@@ -10,42 +10,37 @@ describe('resolveColors', () => {
   });
 
   it('return red colors', () => {
-    expect(resolveColors('red').light).toEqual([
-      colors.R50,
-      colors.R500,
-      colors.R75,
-    ]);
+    expect(resolveColors('red')).toEqual({
+      light: [colors.R50, colors.R500, colors.R75],
+      dark: [colors.R50, colors.R500, colors.R75],
+    });
   });
 
   it('return blue colors', () => {
-    expect(resolveColors('blue').light).toEqual([
-      colors.B50,
-      colors.B500,
-      colors.B75,
-    ]);
+    expect(resolveColors('blue')).toEqual({
+      light: [colors.B50, colors.B500, colors.B75],
+      dark: [colors.B50, colors.B500, colors.B75],
+    });
   });
 
   it('return green colors', () => {
-    expect(resolveColors('green').light).toEqual([
-      colors.G50,
-      colors.G500,
-      colors.G75,
-    ]);
+    expect(resolveColors('green')).toEqual({
+      light: [colors.G50, colors.G500, colors.G75],
+      dark: [colors.G50, colors.G500, colors.G75],
+    });
   });
 
   it('return purple colors', () => {
-    expect(resolveColors('purple').light).toEqual([
-      colors.P50,
-      colors.P500,
-      colors.P75,
-    ]);
+    expect(resolveColors('purple')).toEqual({
+      light: [colors.P50, colors.P500, colors.P75],
+      dark: [colors.P50, colors.P500, colors.P75],
+    });
   });
 
   it('return yellow colors', () => {
-    expect(resolveColors('yellow').light).toEqual([
-      colors.Y50,
-      colors.Y500,
-      colors.Y75,
-    ]);
+    expect(resolveColors('yellow')).toEqual({
+      light: [colors.Y50, colors.Y500, colors.Y75],
+      dark: [colors.Y50, colors.Y500, colors.Y75],
+    });
   });
 });
