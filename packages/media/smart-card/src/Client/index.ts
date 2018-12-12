adb03e52e2f372a71e50e08ff5b62aa0ed8db6f1
Ivan Demchenko <idemchenko@atlassian.com>
2018-12-13 09:59:12 +1100

    temp

--- packages/media/smart-card/src/Client/index.ts
+++ packages/media/smart-card/src/Client/index.ts
@@ -1,11 +1,17 @@
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import { of } from 'rxjs/observable/of';
+import { from } from 'rxjs/observable/from';
 import { merge } from 'rxjs/observable/merge';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import fetch$ from './fetch';
 import { map } from 'rxjs/operators/map';
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged';
 import { delay } from 'rxjs/operators/delay';
 import { takeUntil } from 'rxjs/operators/takeUntil';
+import { filter } from 'rxjs/operators/filter';
+import { mergeMap } from 'rxjs/operators/mergeMap';
+import { tap } from 'rxjs/operators/tap';
 import { catchError } from 'rxjs/operators/catchError';
 import {
   ObjectState,
@@ -128,6 +134,19 @@ export type ClientConfig = {
   loadingStateDelay?: number;
 };
 
+export type ResolveAction = {
+  type: 'RESOLVE';
+  url: string;
+};
+
+export type ReloadAction = {
+  type: 'RELOAD';
+  url: string;
+  definitionIdFromCard?: string;
+};
+
+export type ClientAction = ResolveAction | ReloadAction;
+
 export interface Client {
   fetchData(url: string): Promise<ResolveResponse>;
 }
@@ -136,6 +155,7 @@ export class Client implements Client {
   cacheLifespan: number;
   store: Store<ObjectState>;
   loadingStateDelay: number;
+  urlsToResolve$: Subject<ClientAction>;
 
   constructor(config?: ClientConfig) {
     this.cacheLifespan =
@@ -144,6 +164,75 @@ export class Client implements Client {
       (config && config.getNowTimeFn) || Date.now,
     );
     this.loadingStateDelay = (config && config.loadingStateDelay) || 800;
+    this.urlsToResolve$ = new Subject<ClientAction>();
+
+    const unique$ = this.urlsToResolve$.pipe(distinctUntilChanged());
+
+    const fetchData = <A extends ClientAction>(res: {
+      cmd: A;
+      entry: StateWatch<ObjectState> | undefined;
+    }) => {
+      const data$ = this.startStreaming(res.cmd.url);
+      const resolving$ = of(<ResolvingState>{ status: 'resolving' }).pipe(
+        delay(this.loadingStateDelay),
+        takeUntil(data$),
+      );
+      return merge(resolving$, data$).pipe(
+        map(state => ({ cmd: res.cmd, state })),
+      );
+    };
+
+    unique$
+      .pipe(
+        filter(cmd => cmd.type === 'RESOLVE'),
+        map(cmd => ({
+          cmd: cmd as ResolveAction,
+          entry: this.store.get(cmd.url),
+        })),
+        filter(res => res.entry !== undefined && res.entry.hasExpired()),
+        mergeMap(rec => fetchData<ResolveAction>(rec)),
+      )
+      .subscribe(rec => {
+        this.store.set(rec.cmd.url, rec.state, this.cacheLifespan);
+      });
+
+    unique$
+      .pipe(
+        filter(cmd => cmd.type === 'RELOAD'),
+        map(cmd => ({
+          cmd: cmd as ReloadAction,
+          entry: this.store.get(cmd.url),
+        })),
+        tap(rec => this.store.get(rec.cmd.url)!.invalidate()),
+        mergeMap(rec => fetchData<ReloadAction>(rec)),
+        tap(rec => this.store.set(rec.cmd.url, rec.state, this.cacheLifespan)),
+        mergeMap(rec =>
+          from(
+            getUrlsToReload(this.store, rec.cmd.definitionIdFromCard).filter(
+              otherUrl => otherUrl !== rec.cmd.url,
+            ),
+          ).pipe(mergeMap(x => fetchData(x))),
+        ),
+      )
+      .subscribe();
+
+    // const entry = this.store.get(url);
+
+    // if (entry && entry.hasExpired()) {
+    //   const data$ = this.startStreaming(url);
+    //   const resolving$ = of(<ResolvingState>{ status: 'resolving' }).pipe(
+    //     delay(this.loadingStateDelay),
+    //     takeUntil(data$),
+    //   );
+
+    //   merge(resolving$, data$).subscribe(state => {
+    //     this.store.set(url, state, this.cacheLifespan);
+
+    //     if (cb) {
+    //       cb();
+    //     }
+    //   });
+    // }
   }
 
   fetchData(objectUrl: string): Promise<ResolveResponse> {
@@ -191,41 +280,34 @@ export class Client implements Client {
     return this;
   }
 
-  resolve(url: string, cb?: () => void) {
+  resolve(url: string) {
     if (!this.store.exists(url)) {
       throw new Error('Please, register a smart card before calling get()');
     }
 
-    const entry = this.store.get(url);
-
-    if (entry && entry.hasExpired()) {
-      const data$ = this.startStreaming(url);
-      const resolving$ = of(<ResolvingState>{ status: 'resolving' }).pipe(
-        delay(this.loadingStateDelay),
-        takeUntil(data$),
-      );
-
-      merge(resolving$, data$).subscribe(state => {
-        this.store.set(url, state, this.cacheLifespan);
-
-        if (cb) {
-          cb();
-        }
-      });
-    }
+    this.urlsToResolve$.next({
+      type: 'RESOLVE',
+      url,
+    });
   }
 
-  reload(urlToReload: string, definitionIdFromCard?: string) {
-    this.store.get(urlToReload)!.invalidate();
+  reload(url: string, definitionIdFromCard?: string) {
+    //this.store.get(url)!.invalidate();
 
-    this.resolve(urlToReload, () => {
-      getUrlsToReload(this.store, definitionIdFromCard)
-        .filter(otherUrl => otherUrl !== urlToReload)
-        .forEach(otherUrl => {
-          this.store.get(otherUrl)!.invalidate();
-          this.resolve(otherUrl);
-        });
+    this.urlsToResolve$.next({
+      type: 'RELOAD',
+      url,
+      definitionIdFromCard,
     });
+
+    // this.resolve(urlToReload, () => {
+    //   getUrlsToReload(this.store, definitionIdFromCard)
+    //     .filter(otherUrl => otherUrl !== urlToReload)
+    //     .forEach(otherUrl => {
+    //       this.store.get(otherUrl)!.invalidate();
+    //       this.resolve(otherUrl);
+    //     });
+    // });
   }
 }
 
