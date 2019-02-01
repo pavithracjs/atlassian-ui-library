3752d55b34da2567d89d679e7198d76bdaa80148
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2019-02-01 16:22:56 +1100

    WIP - get styles specific to css properties

--- packages/core/button/examples/Nachos/styles.ts
+++ packages/core/button/examples/Nachos/styles.ts
@@ -1,6 +1,5 @@
 import { colors } from '@atlaskit/theme';
 import { nachosColors } from './colors';
-import { BaseTheme } from '../../src/types';
 
 type NachosBase = {
   padding: string;
@@ -14,122 +13,108 @@ export const nachosBase: NachosBase = {
   height: '32px',
 };
 
-export default <BaseTheme>{
+export const button = {
   background: {
     default: {
-      default: { light: colors.N30 },
-      hover: { light: colors.N40 },
-      active: { light: colors.N800 },
+      default: colors.N30,
+      hover: colors.N40,
+      active: colors.N800,
     },
     primary: {
-      default: { light: nachosColors['green-600'] },
-      hover: { light: nachosColors['green-700'] },
-      active: { light: nachosColors['green-800'] },
+      default: nachosColors['green-600'],
+      hover: nachosColors['green-700'],
+      active: nachosColors['green-800'],
     },
     subtle: {
-      default: { light: colors.N20A },
-      hover: { light: colors.N30A },
-      active: { light: colors.N50A },
+      default: colors.N20A,
+      hover: colors.N30A,
+      active: colors.N50A,
     },
     danger: {
-      default: { light: nachosColors['red-600'] },
-      hover: { light: nachosColors['red-700'] },
-      active: { light: nachosColors['red-800'] },
+      default: nachosColors['red-600'],
+      hover: nachosColors['red-700'],
+      active: nachosColors['red-800'],
     },
-    disabled: {
-      default: { light: colors.N30 },
-    },
+    disabled: colors.N30,
   },
-
   boxShadow: {
     default: {
-      default: { light: `0 1px 0 0 ${colors.N40A}` },
-      hover: { light: `0 1px 0 0 ${colors.N50A}` },
-      active: { light: 'none' },
+      default: `0 1px 0 0 ${colors.N40A}`,
+      hover: `0 1px 0 0 ${colors.N50A}`,
+      active: 'none',
     },
-    primary: {
-      default: { light: `0 1px 0 0 ${nachosColors['green-900']}` },
-    },
-    subtle: {
-      default: { light: 'none' },
-    },
-    danger: {
-      default: { light: `0 1px 0 0 ${nachosColors['red-900']}` },
-    },
-    disabled: {
-      default: { light: 'none' },
-    },
+    primary: `0 1px 0 0 ${nachosColors['green-900']}`,
+    subtle: 'none',
+    danger: `0 1px 0 0 ${nachosColors['red-900']}`,
+    disabled: 'none',
   },
-
   borderColor: {
     default: {
-      default: { light: colors.N40A },
-      hover: { light: colors.N50A },
-      active: { light: colors.N900 },
+      default: colors.N40A,
+      hover: colors.N50A,
+      active: colors.N900,
     },
     primary: {
-      default: { light: nachosColors['green-900'] },
-      hover: { light: nachosColors['green-900'] },
-      active: { light: nachosColors['green-900'] },
+      default: nachosColors['green-900'],
+      hover: nachosColors['green-900'],
+      active: nachosColors['green-900'],
     },
     danger: {
-      default: { light: nachosColors['red-900'] },
-      hover: { light: nachosColors['red-900'] },
-      active: { light: nachosColors['red-900'] },
+      default: nachosColors['red-900'],
+      hover: nachosColors['red-900'],
+      active: nachosColors['red-900'],
     },
   },
-
   fontWeight: {
-    default: {
-      default: { light: 'bold' },
-    },
-    subtle: {
-      default: { light: 'normal' },
-    },
-    disabled: {
-      default: { light: 'normal' },
-    },
+    default: 'bold',
+    subtle: 'normal',
+    disabled: 'normal',
   },
-
   color: {
     default: {
-      default: { light: colors.N700 },
-      active: { light: colors.N0 },
+      default: colors.N700,
+      active: colors.N0,
     },
-    primary: {
-      default: { light: colors.N0 },
-    },
-    subtle: {
-      default: { light: colors.N700 },
-    },
-    danger: {
-      default: { light: colors.N0 },
-    },
-    disabled: {
-      default: { light: colors.N70 },
-    },
+    primary: colors.N0,
+    subtle: colors.N700,
+    danger: colors.N0,
+    disabled: colors.N70,
   },
-
-  border: {
-    default: {
-      default: { light: 'none' },
-    },
-    subtle: {
-      default: { light: 'none' },
-      hover: { light: 'none' },
-      active: { light: 'none' },
-    },
-    disabled: {
-      default: { light: 'none' },
-    },
-  },
-
+  border: 'none',
   cursor: {
-    default: {
-      default: { light: 'pointer' },
-    },
-    disabled: {
-      default: { light: 'not-allowed' },
-    },
+    default: 'pointer',
+    disabled: 'not-allowed',
   },
 };
+
+const getBackground = (background, { appearance, state }) => {
+  if (appearance === 'disabled') return background[appearance];
+  return background[appearance][state];
+};
+
+const getBoxShadow = (boxShadow, { appearance, state }) => {
+  if (appearance === 'default') return boxShadow[appearance][state];
+  return boxShadow[appearance];
+};
+
+const getBorderColor = (borderColor, { appearance, state }) =>
+  borderColor[appearance][state];
+
+const getfontWeight = (fontWeight, { appearance, state }) => fontWeight[state];
+
+const getCursor = (cursor, { state }) => cursor[state];
+
+const getColor = (color, { appearance, state }) => {
+  if (appearance === 'default') return color[appearance][state];
+  return color[appearance];
+};
+
+export const getButtonStyles = props => ({
+  border: button.border,
+  background: getBackground(button.background, props),
+  borderColor: getBorderColor(button.borderColor, props),
+  boxShadow: getBoxShadow(button.boxShadow, props),
+  color: getColor(button.color, props),
+  cursor: getCursor(button.cursor, props),
+  fontWeight: getfontWeight(button.fontWeight, props),
+});
