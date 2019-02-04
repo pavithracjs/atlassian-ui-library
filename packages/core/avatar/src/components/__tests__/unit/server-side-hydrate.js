73e02b18274e76c0b7441745340cda2f37bc259d
Pete Gleeson <pgleeson@atlassian.com>
2019-02-04 17:07:05 +1100

    runs codemod with hydration test

---
+++ packages/core/avatar/src/components/__tests__/unit/server-side-hydrate.js
@@ -0,0 +1,39 @@
+// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
+
+import exenv from 'exenv';
+
+jest.mock('exenv', () => ({
+  get canUseDOM() {
+    return false;
+  },
+}));
+
+jest.spyOn(global.console, 'error');
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should ssr then hydrate avatar correctly', async () => {
+  const [example] = await getExamplesFor('avatar');
+  // $StringLitteral
+  const Example = require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
+  const canUseDom = jest.spyOn(exenv, 'canUseDOM', 'get');
+
+  // server-side
+  canUseDom.mockReturnValue(false);
+  const serverHTML = ReactDOMServer.renderToString(<Example />);
+
+  // client-side
+  canUseDom.mockReturnValue(true);
+  const elem = document.createElement('div');
+  elem.innerHTML = serverHTML;
+
+  ReactDOM.hydrate(<Example />, elem);
+
+  expect(console.error).not.toBeCalled();
+});
