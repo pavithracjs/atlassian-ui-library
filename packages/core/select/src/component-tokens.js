19818edbe710918deeec7bf89d6713e46ff02418
Charles Lee <clee@atlassian.com>
2019-02-15 13:25:35 +1100

    wip select component-tokens

---
+++ packages/core/select/src/component-tokens.js
@@ -0,0 +1,66 @@
+// @flow
+/* eslint-disable camelcase */
+import { colors, gridSize } from '@atlaskit/theme';
+
+export const color_control_border_default = colors.N20;
+export const color_control_border_focused = colors.B100;
+export const color_control_border_error_default = colors.R400;
+export const color_control_border_success = colors.G400;
+export const color_control_border_hover = colors.N30;
+export const color_control_background_hover = colors.N30;
+export const color_control_background_focus = colors.N0;
+export const color_control_scrollbar_hover = 'rgba(0, 0, 0, 0.2)';
+export const animation_transition_backgroundcolor_duration = '200ms';
+export const spacing_control_compact_minheight = gridSize() * 4; // gridSize() = 8;
+export const spacing_control_default_minheight = gridSize() * 5; //
+export const spacing_control_border_radius = '3px';
+
+export const spacing_valueindicator_compact_paddingtop = '0';
+export const spacing_valueindicator_compact_paddingbottom = '0';
+export const spacing_valueindicator_default_paddingtop = '2px';
+export const spacing_valueindicator_default_paddingbottom = '2px';
+
+export const spacing_clearindicator_compact_paddingright = '2px';
+export const spacing_clearindicator_compact_paddingleft = '2px';
+export const spacing_clearindicator_default_paddingright = '2px';
+export const spacing_clearindicator_default_paddingleft = '2px';
+export const spacing_clearindicator_compact_paddingtop = '0';
+export const spacing_clearindicator_compact_paddingbottom = '0';
+export const spacing_clearindicator_default_paddingtop = '6px';
+export const spacing_clearindicator_default_paddingbottom = '6px';
+export const color_clearindicator_text_default = colors.N70;
+export const color_clearindicator_text_hover = colors.N500;
+
+export const spacing_loadingindicator_compact_paddingtop_default = '0';
+export const spacing_loadingindicator_compact_paddingbottom_default = '0';
+export const spacing_loadingindicator_default_paddingtop_default = '6px';
+export const spacing_loadingindicator_default_paddingbottom_default = '6px';
+
+export const color_dropdownindicator_default_text_default = colors.N500;
+export const color_dropdownindicator_default_text_disabled = colors.N70;
+export const color_dropdownindicator_default_text_hover = colors.N200;
+export const spacing_dropdownindicator_compact_paddingtop_default = '0';
+export const spacing_dropdownindicator_compact_paddingbottom_default = '0';
+export const spacing_dropdownindicator_default_paddingtop_default = '6px';
+export const spacing_dropdownindicator_default_paddingbottom_default = '6px';
+export const spacing_dropdownindicator_compact_paddingleft_default = '0';
+export const spacing_dropdownindicator_compact_paddingright_default = '0';
+export const spacing_dropdownindicator_default_paddingleft_default = '2px';
+export const spacing_dropdownindicator_default_paddingright_default = '2px';
+
+export const color_option_default_text_selected = colors.N0; // default inherits from menu
+export const color_option_default_background_selected = colors.N500;
+export const color_option_default_background_focused = colors.N50;
+
+export const spacing_option_default_paddingtop = '6px';
+export const spacing_option_default_paddingbottom = '6px';
+
+export const color_placeholder_default_text_default = colors.N0;
+
+export const color_singlevalue_default_text_default = colors.N800;
+export const color_singlevalue_default_text_disabled = colors.N70;
+
+export const font_singlevalue_default_lineheight_default = `${gridSize() *
+  2}px`; // this should not be a pixel value
+export const spacing_menulist_default_paddingtop_default = gridSize();
+export const spacing_menulist_default_paddingbottom_default = gridSize();
