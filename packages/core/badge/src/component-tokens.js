3d91b41c96e0b939ec4bc6a4779a00ffd47c8269
Charles Lee <clee@atlassian.com>
2019-02-19 15:43:57 +1100

    badge audit

---
+++ packages/core/badge/src/component-tokens.js
@@ -0,0 +1,42 @@
+// @flow
+/* eslint-disable camelcase */
+
+import { colors } from '@atlaskit/theme';
+
+export const spacing_container_default_borderRadius_default = '2em';
+export const typography_container_default_size_default = '12px';
+export const typography_container_default_weight_default = 'normal';
+export const spacing_container_default_lineHeight_default = '1px';
+export const spacing_container_default_minimumwidth_default = '1px';
+export const spacing_container_default_padding_left = '0.5em';
+export const spacing_container_default_padding_right = '0.5em';
+export const spacing_container_default_padding_top = '0.166666666667em';
+export const spacing_container_default_padding_bottom = '0.166666666667em';
+export const spacing_container_default_text_align = 'center';
+
+export const color_container_added_background_default_light = colors.G50;
+export const color_container_added_background_default_dark = colors.G50;
+export const color_container_default_background_default_light = colors.N40;
+export const color_container_default_background_default_dark = colors.DN70;
+export const color_container_important_background_default_light = colors.R400;
+export const color_container_important_background_default_dark = colors.R400;
+export const color_container_primary_background_default_light = colors.B400;
+export const color_container_primary_background_default_dark = colors.B100;
+export const color_container_primaryInverted_background_default_light =
+  colors.N0;
+export const color_container_primaryInverted_background_default_dark =
+  colors.DN400;
+export const color_container_removed_background_default_light = colors.R50;
+export const color_container_removed_background_default_dark = colors.R50;
+export const color_container_added_text_default_light = colors.G500;
+export const color_container_added_text_default_dark = colors.G500;
+export const color_container_default_text_default_light = colors.N800;
+export const color_container_default_text_default_dark = colors.DN900;
+export const color_container_important_text_default_light = colors.N0;
+export const color_container_important_text_default_dark = colors.N0;
+export const color_container_primary_text_default_light = colors.N0;
+export const color_container_primary_text_default_dark = colors.N0;
+export const color_container_primaryInverted_text_default_light = colors.B500;
+export const color_container_primaryInverted_text_default_dark = colors.DN0;
+export const color_container_removed_text_default_light = colors.R500;
+export const color_container_removed_text_default_dark = colors.R500;
