e6717b30e5633b2d52eea44bf8d7879561b6974f
Charles Lee <clee@atlassian.com>
2019-02-19 15:57:42 +1100

    lozenge-audit

---
+++ packages/core/lozenge/src/component-tokens.js
@@ -0,0 +1,73 @@
+// @flow
+/* eslint-disable camelcase */
+import { colors } from '@atlaskit/theme';
+
+export const color_container_default_background_default_light = colors.N40;
+export const color_container_default_background_default_dark = colors.N40;
+export const color_container_inprogress_background_default_light = colors.B50;
+export const color_container_inprogress_background_default_dark = colors.B50;
+export const color_container_moved_background_default_light = colors.Y75;
+export const color_container_moved_background_default_dark = colors.Y75;
+export const color_container_new_background_default_light = colors.P50;
+export const color_container_new_background_default_dark = colors.P50;
+export const color_container_removed_background_default_light = colors.R50;
+export const color_container_removed_background_default_dark = colors.R50;
+export const color_container_success_background_default_light = colors.G50;
+export const color_container_success_background_default_dark = colors.G50;
+
+export const color_container_default_text_default_light = colors.N500;
+export const color_container_default_text_default_dark = colors.N500;
+export const color_container_inprogress_text_default_light = colors.B500;
+export const color_container_inprogress_text_default_dark = colors.B500;
+export const color_container_moved_text_default_light = colors.N800;
+export const color_container_moved_text_default_dark = colors.N800;
+export const color_container_new_text_default_light = colors.P500;
+export const color_container_new_text_default_dark = colors.P500;
+export const color_container_removed_text_default_light = colors.R500;
+export const color_container_removed_text_default_dark = colors.R500;
+export const color_container_success_text_default_light = colors.G500;
+export const color_container_success_text_default_dark = colors.G500;
+
+export const color_container_bolddefault_background_default_light = colors.N500;
+export const color_container_bolddefault_background_default_dark = colors.N500;
+export const color_container_boldinprogress_background_default_light =
+  colors.B400;
+export const color_container_boldinprogress_background_default_dark =
+  colors.B400;
+export const color_container_boldmoved_background_default_light = colors.Y500;
+export const color_container_boldmoved_background_default_dark = colors.Y500;
+export const color_container_boldnew_background_default_light = colors.P400;
+export const color_container_boldnew_background_default_dark = colors.P400;
+export const color_container_boldremoved_background_default_light = colors.R400;
+export const color_container_boldremoved_background_default_dark = colors.R400;
+export const color_container_boldsuccess_background_default_light = colors.G400;
+export const color_container_boldsuccess_background_default_dark = colors.G400;
+
+export const color_container_bolddefault_text_default_light = colors.N0;
+export const color_container_bolddefault_text_default_dark = colors.N0;
+export const color_container_boldinprogress_text_default_light = colors.N800;
+export const color_container_boldinprogress_text_default_dark = colors.N800;
+export const color_container_boldmoved_text_default_light = colors.N0;
+export const color_container_boldmoved_text_default_dark = colors.N0;
+export const color_container_boldnew_text_default_light = colors.N0;
+export const color_container_boldnew_text_default_dark = colors.N0;
+export const color_container_boldremoved_text_default_light = colors.N0;
+export const color_container_boldremoved_text_default_dark = colors.N0;
+export const color_container_boldsuccess_text_default_light = colors.N0;
+export const color_container_boldsuccess_text_default_dark = colors.N0;
+
+export const spacing_content_default_padding_top = '0px';
+export const spacing_content_default_padding_bottom = '0px';
+export const spacing_content_default_padding_left = '4px';
+export const spacing_content_default_padding_right = '4px';
+export const spacing_content_default_width = '100%';
+export const spacing_content_default_maximumwidth = '200px';
+
+export const spacing_container_default_borderRadius_default = '3px';
+export const typography_container_default_size_default = '11px';
+export const typography_container_default_weight_default = '1px';
+export const spacing_container_default_minimumwidth_default = '100%';
+export const spacing_container_default_padding_top = '2px';
+export const spacing_container_default_padding_left = '0px';
+export const spacing_container_default_padding_bottom = '3px';
+export const spacing_container_default_padding_right = '0px';
