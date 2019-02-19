db9a72dc07c49a4c9b35b150d05b259e62d3bb16
Charles Lee <clee@atlassian.com>
2019-02-19 15:24:19 +1100

    checkbox audit

---
+++ packages/core/checkbox/src/component-tokens.js
@@ -0,0 +1,65 @@
+// @flow
+/* eslint-disable camelcase */
+import { colors } from '@atlaskit/theme';
+
+export const color_label_default_text_disabled_dark = colors.N80;
+export const color_label_default_text_disabled_light = colors.N80;
+export const color_label_default_text_default_light = colors.N900;
+export const color_label_default_text_default_dark = colors.DN60;
+
+export const color_checkbox_default_border_default_light = colors.N40;
+export const color_checkbox_default_border_default_dark = colors.DN80;
+export const color_checkbox_default_border_focus_light = colors.B100;
+export const color_checkbox_default_border_focus_dark = colors.B75;
+export const color_checkbox_default_border_invalid_light = colors.R300;
+export const color_checkbox_default_border_invalid_dark = colors.R300;
+export const color_checkbox_default_border_active_light = color_checkbox_default_border_default_light;
+export const color_checkbox_default_border_active_dark = color_checkbox_default_border_default_dark;
+export const color_checkbox_default_border_checked_light = color_checkbox_default_border_default_light;
+export const color_checkbox_default_border_checked_dark = color_checkbox_default_border_default_dark;
+export const color_checkbox_default_border_hovered_light = colors.N40;
+export const color_checkbox_default_border_hovered_dark = colors.DN200;
+export const color_checkbox_default_border_disabled_light = '';
+export const color_checkbox_default_border_disabled_dark = '';
+
+export const color_checkbox_default_tick_checked_light = colors.N10;
+export const color_checkbox_default_tick_checked_dark = colors.DN10;
+export const color_checkbox_default_tick_defaultAndChecked_light = colors.N70;
+export const color_checkbox_default_tick_defaultAndChecked_dark = colors.DN90;
+export const color_checkbox_default_tick_activeAndChecked_light = colors.B400;
+export const color_checkbox_default_tick_activeAndChecked_dark = colors.DN10;
+export const color_checkbox_default_tick_default_light = 'transparent';
+export const color_checkbox_default_tick_default_dark = 'transparent';
+
+export const color_checkbox_default_box_default_light = colors.N10;
+export const color_checkbox_default_box_default_dark = colors.DN10;
+export const color_checkbox_default_box_disabled_light = colors.N20;
+export const color_checkbox_default_box_disabled_dark = colors.DN10;
+export const color_checkbox_default_box_active_light = colors.B50;
+export const color_checkbox_default_box_active_dark = colors.B200;
+export const color_checkbox_default_box_hoveredAndChecked_light = colors.B300;
+export const color_checkbox_default_box_hoveredAndChecked_dark = colors.B75;
+export const color_checkbox_default_box_hovered_light = colors.N30;
+export const color_checkbox_default_box_hovered_dark = colors.DN30;
+export const color_checkbox_default_box_checked_light = colors.B400;
+export const color_checkbox_default_box_checked_dark = colors.B400;
+
+export const spacing_label_default_padding_top = '2px';
+export const spacing_label_default_padding_bottom = '2px';
+export const spacing_label_default_padding_left = '4px';
+export const spacing_label_default_padding_right = '4px';
+
+export const spacing_checkbox_default_box_lineHeight = 0;
+export const animation_checkbox_default_box_property = 'all';
+export const animation_checkbox_default_box_duration = '0.2s';
+export const animation_checkbox_default_box_timingFunction = 'ease-in-out';
+export const animation_checkbox_default_box_delay = 0;
+
+export const animation_checkbox_default_border_property = 'stroke';
+export const animation_checkbox_default_border_duration = '0.2s';
+export const animation_checkbox_default_border_timingFunction = 'ease-in-out';
+export const animation_checkbox_default_border_delay = 0;
+
+export const color_requireIndicator_default_text_default_light = colors.R400;
+export const color_requireIndicator_default_text_default_dark = colors.R400;
+export const spacing_requiredIndicator_default_padding_left = '2px';
