b064b4104af5856d2aedf4404013551d3b4d5973
Charles Lee <clee@atlassian.com>
2019-02-19 13:21:18 +1100

    radio-audit

---
+++ packages/core/radio/src/component-tokens.js
@@ -0,0 +1,60 @@
+// @flow
+/* eslint-disable camelcase */
+import { colors } from '@atlaskit/theme';
+
+export const color_label_default_text_default_dark = colors.N80;
+export const color_label_default_text_default_light = colors.N80;
+export const color_label_default_text_disabled_dark = colors.N900;
+export const color_label_default_text_disabled_light = colors.DN60;
+
+export const color_radio_default_border_default_dark = colors.DN80;
+export const color_radio_default_border_default_light = colors.N40;
+export const color_radio_default_border_focus_light = colors.B100;
+export const color_radio_default_border_focus_dark = colors.DN80;
+export const color_radio_default_border_invalid_light = colors.R300;
+export const color_radio_default_border_invalid_dark = colors.R300;
+export const color_radio_default_border_hover_light = colors.N40;
+export const color_radio_default_border_hover_dark = colors.DN200;
+
+export const color_radio_default_border_active_dark = color_radio_default_border_default_dark;
+export const color_radio_default_border_active_light = color_radio_default_border_default_light;
+export const color_radio_default_border_checked_light = color_radio_default_border_default_light;
+export const color_radio_default_border_checked_dark = color_radio_default_border_default_dark;
+
+export const color_radio_default_dot_default_light = colors.N10;
+export const color_radio_default_dot_default_dark = colors.DN90;
+export const color_radio_default_dot_active_light = 'transparent';
+export const color_radio_default_dot_active_dark = 'transparent';
+export const color_radio_default_dot_disabledAndChecked_light = colors.N70;
+export const color_radio_default_dot_disabledAndChecked_dark = colors.DN90;
+export const color_radio_default_dot_activeAndChecked_light = colors.B400;
+export const color_radio_default_dot_activeAndChecked_dark = colors.DN10;
+
+export const color_radio_default_circle_default_light = colors.N10;
+export const color_radio_default_circle_default_dark = colors.DN10;
+export const color_radio_default_circle_disabled_light = colors.N20;
+export const color_radio_default_circle_disabled_dark = colors.DN10;
+export const color_radio_default_circle_active_light = colors.B50;
+export const color_radio_default_circle_active_dark = colors.B200;
+export const color_radio_default_circle_hover_light = colors.N30;
+export const color_radio_default_circle_hover_dark = colors.DN30;
+export const color_radio_default_circle_checkedAndHovered_light = colors.B300;
+export const color_radio_default_circle_checkedAndHovered_dark = colors.B75;
+export const color_radio_default_circle_checked_light = colors.B400;
+export const color_radio_default_circle_checked_dark = colors.B400;
+export const color_radio_default_circle_checkedInvalid_dark = colors.DN10;
+
+export const spacing_radio_default_dot_lineheight = 0;
+export const animation_radio_default_dot_property = 'all';
+export const animation_radio_default_dot_duration = '0.2s';
+export const animation_radio_default_dot_timingFunction = 'ease-in-out';
+export const animation_radio_default_dot_delay = 0;
+export const animation_radio_default_circle_property = 'stroke';
+export const animation_radio_default_circle_duration = '0.2s';
+export const animation_radio_default_circle_timingFunction = 'ease-in-out';
+export const animation_radio_default_circle_delay = 0;
+
+export const spacing_label_default_padding_left = '4px';
+export const spacing_label_default_padding_right = '4px';
+export const spacing_label_default_padding_top = '2px';
+export const spacing_label_default_padding_bottom = '2px';
