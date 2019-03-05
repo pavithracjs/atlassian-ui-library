bd9c193699bd4d3d719bcaded344f200671af810
Sarah Federman <sfederman@atlassian.com>
2019-03-05 11:49:37 -0800

    added flag tokens

---
+++ packages/core/flag/src/component-tokens.js
@@ -0,0 +1,106 @@
+// @flow
+/* eslint-disable camelcase */
+import { colors } from '@atlaskit/theme';
+
+// Flag
+// note: changed normal mode to 'default'
+export const color_flag_error_background_default_light = colors.R400;
+export const color_flag_error_background_default_dark = colors.R300;
+export const color_flag_info_background_default_light = colors.N500;
+export const color_flag_info_background_default_dark = colors.N500;
+export const color_flag_default_background_default_light = colors.N0;
+export const color_flag_default_background_default_dark = colors.DN50;
+export const color_flag_success_background_default_light = colors.G400;
+export const color_flag_success_background_default_dark = colors.G300;
+export const color_flag_warning_background_default_light = colors.Y200;
+export const color_flag_warning_background_default_dark = colors.Y300;
+
+export const color_flag_default_border_default_light = colors.N60A;
+
+export const color_flag_error_text_default_light = colors.N0;
+export const color_flag_error_text_default_dark = colors.DN40;
+export const color_flag_info_text_default_light = colors.N0;
+export const color_flag_info_text_default_dark = colors.DN600;
+export const color_flag_default_text_default_light = colors.N500;
+export const color_flag_default_text_default_dark = colors.DN600;
+export const color_flag_success_text_default_light = colors.N0;
+export const color_flag_success_text_default_dark = colors.DN40;
+export const color_flag_warning_text_default_light = colors.N700;
+export const color_flag_warning_text_default_dark = colors.DN40;
+
+export const color_flag_error_shadow_default_light = colors.N50A;
+export const color_flag_error_shadow_default_dark = colors.N50A;
+export const color_flag_info_shadow_default_light = colors.N50A;
+export const color_flag_info_shadow_default_dark = colors.N50A;
+export const color_flag_default_shadow_default_light = colors.N50A;
+export const color_flag_default_shadow_default_dark = colors.N50A;
+export const color_flag_success_shadow_default_light = colors.N50A;
+export const color_flag_success_shadow_default_dark = colors.N50A;
+export const color_flag_warning_shadow_default_light = colors.N50A;
+export const color_flag_warning_shadow_default_dark = colors.N50A;
+
+export const color_flag_error_focusRing_default_light = colors.N40;
+export const color_flag_error_focusRing_default_dark = colors.N40;
+export const color_flag_info_focusRing_default_light = colors.N40;
+export const color_flag_info_focusRing_default_dark = colors.N40;
+export const color_flag_default_focusRing_default_light = colors.B100;
+export const color_flag_default_focusRing_default_dark = colors.link;
+export const color_flag_success_focusRing_default_light = colors.N40;
+export const color_flag_success_focusRing_default_dark = colors.N40;
+export const color_flag_warning_focusRing_default_light = colors.N200;
+export const color_flag_warning_focusRing_default_dark = colors.N200;
+
+// Button
+// No custom button theme for default variant. Didn't add default button theme values here
+export const typography_button_success_textdecoration_hover = 'underline';
+export const typography_button_success_textdecoration_active = 'underline';
+export const typography_button_info_textdecoration_hover = 'underline';
+export const typography_button_info_textdecoration_active = 'underline';
+export const typography_button_error_textdecoration_hover = 'underline';
+export const typography_button_error_textdecoration_active = 'underline';
+export const typography_button_warning_textdecoration_hover = 'underline';
+export const typography_button_warning_textdecoration_active = 'underline';
+
+export const color_button_success_text_default_light = colors.N0;
+export const color_button_success_text_default_dark = colors.DN40;
+export const color_button_success_text_hover_light = colors.N0;
+export const color_button_success_text_hover_dark = colors.DN40;
+export const color_button_success_text_active_light = colors.N0;
+export const color_button_success_text_active_dark = colors.DN40;
+
+export const color_button_success_background_default_light =
+  'rgba(255, 255, 255, 0.08)';
+export const color_button_success_background_default_dark = colors.N30A;
+
+export const color_button_info_text_default_light = colors.N0;
+export const color_button_info_text_default_dark = colors.DN600;
+export const color_button_info_text_hover_light = colors.N0;
+export const color_button_info_text_hover_dark = colors.DN600;
+export const color_button_info_text_active_light = colors.N0;
+export const color_button_info_text_active_dark = colors.DN600;
+
+export const color_button_info_background_default_light =
+  'rgba(255, 255, 255, 0.08)';
+export const color_button_info_background_default_dark =
+  'rgba(255, 255, 255, 0.08)';
+
+export const color_button_error_text_default_light = colors.N0;
+export const color_button_error_text_default_dark = colors.DN600;
+export const color_button_error_text_hover_light = colors.N0;
+export const color_button_error_text_hover_dark = colors.DN600;
+export const color_button_error_text_active_light = colors.N0;
+export const color_button_error_text_active_dark = colors.DN600;
+
+export const color_button_error_background_default_light =
+  'rgba(255, 255, 255, 0.08)';
+export const color_button_error_background_default_dark = colors.N30A;
+
+export const color_button_warning_text_default_light = colors.N700;
+export const color_button_warning_text_default_dark = colors.DN40;
+export const color_button_warning_text_hover_light = colors.N700;
+export const color_button_warning_text_hover_dark = colors.DN40;
+export const color_button_warning_text_active_light = colors.N700;
+export const color_button_warning_text_active_dark = colors.DN40;
+
+export const color_button_warning_background_default_light = colors.N30A;
+export const color_button_warning_background_default_dark = colors.N30A;
