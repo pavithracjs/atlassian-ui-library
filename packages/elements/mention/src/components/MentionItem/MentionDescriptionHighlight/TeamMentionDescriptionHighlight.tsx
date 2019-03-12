f79a0745c5ce6d83d60f7ca72565bd8f8914eb64
Ishan Somasiri <isomasiri@atlassian.com>
2019-03-12 12:31:34 +1100

    TEAMS-319 : Comment added

--- packages/elements/mention/src/components/MentionItem/MentionDescriptionHighlight/TeamMentionDescriptionHighlight.tsx
+++ packages/elements/mention/src/components/MentionItem/MentionDescriptionHighlight/TeamMentionDescriptionHighlight.tsx
@@ -3,33 +3,37 @@ import { FormattedMessage } from 'react-intl';
 import { Props } from './types';
 import { TeamInformationStyle } from './styles';
 
-function renderTeamInformation(memberCount?: number, includesYou?: boolean) {
-  // todo - refactor with TeamOption ?
-  // if Member count is missing, do not show the byline, regardless of the availability of includesYou
-  if (memberCount === null || typeof memberCount === 'undefined') {
-    return undefined;
-  }
-  return (
-    <TeamInformationStyle>
-      <FormattedMessage
-        {...(memberCount > 50 ? messages.plus50Members : messages.memberCount)}
-        values={{ count: memberCount, includes: includesYou }}
-      />
-    </TeamInformationStyle>
-  );
-}
-
 export default class TeamMentionDescriptionHighlight extends React.PureComponent<
   Props,
   {}
 > {
+  renderTeamInformation(memberCount?: number, includesYou?: boolean) {
+    // TEAMS-333 This same logic is implemented in User Picker as well. https://product-fabric.atlassian.net/browse/TEAMS-333
+    // will extract this to a common place
+
+    // if Member count is missing, do not show the byline, regardless of the availability of includesYou
+    if (memberCount === null || typeof memberCount === 'undefined') {
+      return undefined;
+    }
+    return (
+      <TeamInformationStyle>
+        <FormattedMessage
+          {...(memberCount > 50
+            ? messages.plus50Members
+            : messages.memberCount)}
+          values={{ count: memberCount, includes: includesYou }}
+        />
+      </TeamInformationStyle>
+    );
+  }
+
   render() {
     const { context } = this.props.mention;
 
     const includesYou = context && context.includesYou;
     const memberCount = context && context.memberCount;
 
-    return renderTeamInformation(memberCount, includesYou);
+    return this.renderTeamInformation(memberCount, includesYou);
   }
 }
 
