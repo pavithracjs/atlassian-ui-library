909f9e5f52ba5ce15294034051e3b5f35f8cb924
Kang Wei Chan <kchan@atlassian.com>
2019-03-13 16:11:32 +1100

    Change class component to function

--- packages/core/textfield/examples/05-elements-before-and-after.js
+++ packages/core/textfield/examples/05-elements-before-and-after.js
@@ -1,25 +1,23 @@
 // @flow
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import ErrorIcon from '@atlaskit/icon/glyph/error';
 import Avatar from '@atlaskit/avatar';
 import Textfield from '../src';
 
-export default class TextfieldExample extends Component<{}> {
-  render() {
-    return (
-      <Fragment>
-        <label htmlFor="after-input">After input</label>
-        <Textfield id="after-input" elemAfterInput={<ErrorIcon />} />
-        <label htmlFor="before-input">Before input</label>
-        <Textfield
-          id="before-input"
-          elemBeforeInput={
-            <div style={{ paddingRight: '4px', lineHeight: '100%' }}>
-              <Avatar size="small" borderColor="transparent" />
-            </div>
-          }
-        />
-      </Fragment>
-    );
-  }
+export default function() {
+  return (
+    <Fragment>
+      <label htmlFor="after-input">After input</label>
+      <Textfield id="after-input" elemAfterInput={<ErrorIcon />} />
+      <label htmlFor="before-input">Before input</label>
+      <Textfield
+        id="before-input"
+        elemBeforeInput={
+          <div style={{ paddingRight: '4px', lineHeight: '100%' }}>
+            <Avatar size="small" borderColor="transparent" />
+          </div>
+        }
+      />
+    </Fragment>
+  );
 }
