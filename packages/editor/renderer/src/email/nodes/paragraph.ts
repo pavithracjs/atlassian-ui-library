a63db260cdfb7c8b35ae69e06cd44e9a1eeb96dd
Nathan Pickworth <nathan.pickworth@siteminder.com>
2019-03-14 11:51:49 +1100

    serializeStlye fix for p tag

--- packages/editor/renderer/src/email/nodes/paragraph.ts
+++ packages/editor/renderer/src/email/nodes/paragraph.ts
@@ -11,17 +11,21 @@ const style = serializeStyle({
   margin: `0px`,
 });
 
-const tableStyle = {
+const tableStyle = serializeStyle({
   ...commonStyle,
   margin: 0,
   padding: 0,
   width: '100%',
   'border-spacing': '0px',
-};
+});
 
 export default function paragraph({ text, marks }: NodeSerializerOpts) {
   const paragraphTd = createTag('td', { style }, text);
-  const paragraphAsTable = createTag('table', tableStyle, paragraphTd);
+  const paragraphAsTable = createTag(
+    'table',
+    { style: tableStyle },
+    paragraphTd,
+  );
 
   return applyMarks(marks, paragraphAsTable);
 }
