3c55f157b953b6ff73a891975cfc3dcddcd4cd8d
Marco de Jongh <mdejongh@atlassian.com>
2019-03-14 17:47:11 +1100

    WIP Code mode for reworkign theme imports

---
+++ packages/core/theme/codemods/rework-imports-to-use-alternative-entries.js
@@ -0,0 +1,109 @@
+//@flow
+/* 
+WIP This codemod is not ready yet to be used on products
+*/
+
+const themeIndexImports = [
+  'themed',
+  'withTheme',
+  'AtlaskitThemeProvider',
+  'getTheme',
+  'createTheme',
+];
+
+const constants = [
+  'gridSize',
+  'FLATTENED',
+  'CHANNEL',
+  'DEFAULT_THEME_MODE',
+  'THEME_MODES',
+  'borderRadius',
+  'gridSize',
+  'fontSize',
+  'fontSizeSmall',
+  'fontFamily',
+  'codeFontFamily',
+  'focusRing',
+  'noFocusRing',
+  'layers',
+  'assistive',
+];
+
+const akTheme = '@atlaskit/theme';
+
+const constantsPredicate = specifier =>
+  !specifier ||
+  !specifier.imported ||
+  constants.indexOf(specifier.imported.name) > -1;
+
+function getConstantsImport(j, path) {
+  const constantsSpecifierspath = path.value.specifiers.filter(
+    constantsPredicate,
+  );
+
+  if (constantsSpecifierspath.length === 0) {
+    return;
+  }
+
+  return j.importDeclaration(
+    constantsSpecifierspath,
+    j.literal(`${akTheme}/constants`),
+  );
+}
+const indexPredicate = specifier =>
+  !specifier ||
+  !specifier.imported ||
+  themeIndexImports.indexOf(specifier.imported.name) > -1;
+
+function getIndexImport(j, path) {
+  const mainIndexSpecifierspath = path.value.specifiers.filter(indexPredicate);
+
+  if (mainIndexSpecifierspath.length === 0) {
+    return;
+  }
+
+  return j.importDeclaration(
+    mainIndexSpecifierspath,
+    j.literal(`${akTheme}/components`),
+  );
+}
+
+function getOtherImports(j, path) {
+  return path.value.specifiers
+    .filter(
+      specifier => !indexPredicate(specifier) && !constantsPredicate(specifier),
+    )
+    .map(specifier =>
+      j.importDeclaration(
+        [j.importNamespaceSpecifier(j.identifier(specifier.imported.name))],
+        j.literal(`${akTheme}/${specifier.imported.name}`),
+      ),
+    );
+}
+
+export default function transformer(file, api) {
+  const j = api.jscodeshift;
+  const akTheme = '@atlaskit/theme';
+
+  return j(file.source)
+    .find(j.ImportDeclaration)
+    .filter(path => path.node.source.value === akTheme)
+    .forEach(path => {
+      const [firstImport, ...importsAfter] = [
+        getIndexImport(j, path),
+        getConstantsImport(j, path),
+        ...getOtherImports(j, path),
+      ].filter(importStat => importStat);
+
+      if (!firstImport) {
+        return;
+      }
+
+      firstImport.comments = path.value.comments;
+
+      j(path)
+        .replaceWith(firstImport)
+        .insertAfter(importsAfter);
+    })
+    .toSource();
+}
