851f1fce6804a75bb75a4f00b8ab247b0b170d7d
Ishan Somasiri <isomasiri@atlassian.com>
2019-03-18 14:30:58 +1100

    TEAMS-319 : Moved i18n's

--- packages/elements/mention/src/components/MentionDescriptionHighlight/TeamMentionDescriptionHighlight.tsx
+++ packages/elements/mention/src/components/MentionDescriptionHighlight/TeamMentionDescriptionHighlight.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { DescriptionHighlightProps } from './types';
 import { DescriptionHighlightStyle } from './styles';
+import { messages } from '../i18n';
 
 export default class TeamMentionDescriptionHighlight extends React.PureComponent<
   DescriptionHighlightProps,
@@ -37,20 +38,3 @@ export default class TeamMentionDescriptionHighlight extends React.PureComponent
     return this.renderTeamInformation(memberCount, includesYou);
   }
 }
-
-const messages = {
-  memberCount: {
-    id: 'fabric.elements.mentions.team.member.count',
-    defaultMessage:
-      '{count} {count, plural, one {member} other {members}}{includes, select, true {, including you} other {}}',
-    description:
-      'Number of members in the team and whether it includes the current user',
-  },
-  plus50Members: {
-    id: 'fabric.elements.mentions.team.member.50plus',
-    defaultMessage:
-      '50+ members{includes, select, true {, including you} other {}}',
-    description:
-      'Number of members in a team exceeds 50 and whether it includes the current user',
-  },
-};
