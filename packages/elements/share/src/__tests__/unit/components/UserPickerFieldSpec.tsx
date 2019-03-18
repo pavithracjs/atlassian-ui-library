c6bec1af713feb8321469c1a8c5f01b8e5b176e4
ttjandra <ttjandra@atlassian.com>
2019-03-18 11:28:28 +1100

    FS-3548 fix rename in share package

--- packages/elements/share/src/__tests__/unit/components/UserPickerFieldSpec.tsx
+++ packages/elements/share/src/__tests__/unit/components/UserPickerFieldSpec.tsx
@@ -58,7 +58,7 @@ describe('UserPickerField', () => {
     ).find(UserPicker);
     expect(userPicker).toHaveLength(1);
     expect(userPicker.props()).toMatchObject({
-      context: 'share',
+      fieldId: 'share',
       addMoreMessage: 'add more',
       onChange: fieldProps.onChange,
       value: fieldProps.value,
