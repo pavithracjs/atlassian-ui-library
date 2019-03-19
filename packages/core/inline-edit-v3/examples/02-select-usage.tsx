f955517be36b6defbc76de31b75d29919513376a
Kang Wei Chan <kchan@atlassian.com>
2019-03-19 14:38:52 +1100

    Add select example

---
+++ packages/core/inline-edit-v3/examples/02-select-usage.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import Select from '@atlaskit/select';
+import Group from '@atlaskit/tag-group';
+import Tag from '@atlaskit/tag';
+
+import InlineEdit from '../src';
+import ReadViewContainer from './styled/ReadViewContainer';
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+const selectOptions: Option[] = [
+  { label: 'Apple', value: 'Apple' },
+  { label: 'Banana', value: 'Banana' },
+  { label: 'Cherry', value: 'Cherry' },
+  { label: 'Mango', value: 'Mango' },
+  { label: 'Orange', value: 'Orange' },
+  { label: 'Strawberry', value: 'Strawberry' },
+  { label: 'Watermelon', value: 'Watermelon' },
+];
+
+type State = {
+  editValue: Option[];
+  isEditing: boolean;
+  onEventResult: string;
+};
+
+export default class InlineEditExample extends React.Component<void, State> {
+  editViewRef: HTMLInputElement | undefined;
+
+  state = {
+    isEditing: false,
+    editValue: [],
+  };
+
+  onConfirm = (value: Option[]) => {
+    this.setState({
+      editValue: value,
+      isEditing: false,
+    });
+  };
+
+  onCancel = () => {
+    this.setState({ isEditing: false });
+  };
+
+  onEditRequested = () => {
+    this.setState({ isEditing: true }, () => {
+      if (this.editViewRef) {
+        this.editViewRef.focus();
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ padding: '0 16px' }}>
+        <InlineEdit
+          defaultValue={this.state.editValue}
+          label="Inline Edit Field"
+          editView={fieldProps => (
+            <Select
+              {...fieldProps}
+              options={selectOptions}
+              isMulti
+              autoFocus
+              openMenuOnFocus
+            />
+          )}
+          readView={
+            this.state.editValue.length === 0 ? (
+              <ReadViewContainer>Click to enter value</ReadViewContainer>
+            ) : (
+              <Group>
+                {this.state.editValue.map((option: Option) => (
+                  <Tag text={option.label} key={option.label} />
+                ))}
+              </Group>
+            )
+          }
+          onConfirm={this.onConfirm}
+          onCancel={this.onCancel}
+          isEditing={this.state.isEditing}
+          onEditRequested={this.onEditRequested}
+        />
+      </div>
+    );
+  }
+}
