2a9836bb06b95a2062412f7702eab27d9dbeaa4e
phuc_thi_minh_nguyen <>
2019-03-20 16:29:40 +1100

    change to use global navigation example

---
+++ packages/core/global-navigation/examples/basic-global-navigation-using-ai.js
@@ -0,0 +1,47 @@
+// @flow
+
+import React from 'react';
+import EmojiAtlassianIcon from '@atlaskit/icon/glyph/emoji/atlassian';
+import {
+  GlobalItem,
+  LayoutManager,
+  NavigationProvider,
+} from '@atlaskit/navigation-next';
+import AppSwitcherIcon from '@atlaskit/icon/glyph/app-switcher';
+
+import GlobalNavigation from '../src';
+
+const AppSwitcherComponent = props => (
+  <GlobalItem
+    {...props}
+    icon={AppSwitcherIcon}
+    id="test"
+    onClick={() => console.log('AppSwitcher clicked')}
+  />
+);
+
+// TODO: make onClicks targets show up on page instead of console.logs
+const Global = () => (
+  <GlobalNavigation
+    productIcon={EmojiAtlassianIcon}
+    productHref="#"
+    onProductClick={() => alert('Product clicked')}
+    onCreateClick={() => alert('create clicked')}
+    onSearchClick={() => alert('search clicked')}
+    onStarredClick={() => alert('starred clicked')}
+    onNotificationClick={() => alert('notification clicked')}
+    onSettingsClick={() => alert('settings clicked')}
+  />
+);
+
+export default () => (
+  <NavigationProvider>
+    <LayoutManager
+      globalNavigation={Global}
+      productNavigation={() => null}
+      containerNavigation={() => null}
+    >
+      <div css={{ padding: '32px 40px' }}>Page content</div>
+    </LayoutManager>
+  </NavigationProvider>
+);
