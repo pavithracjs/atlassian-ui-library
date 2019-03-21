22a2d7c9ba6f9d24e47edfa15844fbf2c6e2d6fd
Patrick Lai <plai@atlassian.com>
2019-03-21 14:54:09 +1100

    refactor: migrate Checkbox unit tests to TS
    revert change to droplist

--- packages/core/checkbox/src/__tests__/unit/server-side-hydrate.tsx
+++ packages/core/checkbox/src/__tests__/unit/server-side-hydrate.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
 import { ssr } from '@atlaskit/ssr';
 
-jest.spyOn(global.console, 'error').mockImplementation(() => {});
+jest.spyOn(global.console, 'error');
 
 afterEach(() => {
   jest.resetAllMocks();
@@ -11,22 +11,11 @@ afterEach(() => {
 
 test('should ssr then hydrate checkbox correctly', async () => {
   const [example] = await getExamplesFor('checkbox');
-  const Example = require(example.filePath).default; // tslint:disable-line:import/no-dynamic-require
+  const Example = require(example.filePath).default; // tslint-disable-line import/no-dynamic-require
 
   const elem = document.createElement('div');
   elem.innerHTML = await ssr(example.filePath);
 
   ReactDOM.hydrate(<Example />, elem);
-  // ignore warnings caused by emotion's server-side rendering approach
-  // eslint-disable-next-line no-console
-  const mockCalls = console.error.mock.calls.filter(
-    ([f, s]) =>
-      !(
-        f ===
-          'Warning: Did not expect server HTML to contain a <%s> in <%s>.' &&
-        s === 'style'
-      ),
-  );
-
-  expect(mockCalls.length).toBe(0); // eslint-disable-line no-console
+  expect(console.error).not.toBeCalled(); // tslint-disable-line no-console
 });
