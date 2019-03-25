c6f11e208c2046037c95b3102a5f78a343b3b5c4
Fred Ghilini <fghilini@atlassian.com>
2019-03-26 10:57:24 +1100

    ED-6488: fixed param name passing dark mode

--- packages/editor/editor-mobile-bridge/src/editor/mobile-editor-element.tsx
+++ packages/editor/editor-mobile-bridge/src/editor/mobile-editor-element.tsx
@@ -72,7 +72,7 @@ type Props = EditorProps & {
 
 export default function mobileEditor(props: Props) {
   // eg. If the URL parameter is like ?mode=dark use that, otherwise check the prop (used in example)
-  const mode = (params && params.mode) || props.mode || 'light';
+  const mode = (params && params.theme) || props.mode || 'light';
   return (
     <SmartCardProvider>
       <AtlaskitThemeProvider mode={mode}>
