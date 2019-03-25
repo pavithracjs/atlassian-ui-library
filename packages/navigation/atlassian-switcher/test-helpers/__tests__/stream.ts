0b08bbebf24c0c5c6c89304983989935d3fc3a82
Mario Maradiaga <mmaradiaga@atlassian.com>
2019-03-25 17:02:07 +1100

    Addressing PR feedback

--- packages/navigation/atlassian-switcher/test-helpers/__tests__/stream.ts
+++ packages/navigation/atlassian-switcher/test-helpers/__tests__/stream.ts
@@ -2,85 +2,69 @@ import createStream from '../stream';
 
 describe('event-stream', () => {
   describe('next', () => {
-    it('should wait for the next call entering the stream', done => {
+    it('should wait for the next call entering the stream', async () => {
       const stream = createStream();
       const sentValue = { some: 'value' };
-      stream.next().then(valueFromStream => {
-        expect(valueFromStream).toBe(sentValue);
-        done();
-      });
+      const promiseFromStream = stream.next();
       stream(sentValue);
+      const valueFromStream = await promiseFromStream;
+      expect(valueFromStream).toBe(sentValue);
     });
-    it('should be able to return values already sent to the stream', done => {
+    it('should be able to return values already sent to the stream', async () => {
       const stream = createStream();
       const sentValue = { some: 'value' };
       stream(sentValue);
-      stream.next().then(valueFromStream => {
-        expect(valueFromStream).toBe(sentValue);
-        done();
-      });
+      const valueFromStream = await stream.next();
+      expect(valueFromStream).toBe(sentValue);
     });
   });
   describe('skip', () => {
-    it('should be able to return the skipped values entering the stream', done => {
+    it('should be able to return the skipped values entering the stream', async () => {
       const stream = createStream();
       const skippedValues = ['a', 'b', 'c'];
-      stream.skip(3).then(resolvedSkippedValues => {
-        expect(resolvedSkippedValues).toMatchObject(skippedValues);
-        done();
-      });
+      const resolvedSkippedValuesPromise = stream.skip(3);
       stream(skippedValues[0]);
       stream(skippedValues[1]);
       stream(skippedValues[2]);
+      const resolvedSkippedValues = await resolvedSkippedValuesPromise;
+      expect(resolvedSkippedValues).toMatchObject(skippedValues);
     });
-    it('should be able to return the skipped values already sent to the stream', done => {
+    it('should be able to return the skipped values already sent to the stream', async () => {
       const stream = createStream();
       const skippedValues = ['a', 'b', 'c'];
       stream(skippedValues[0]);
       stream(skippedValues[1]);
       stream(skippedValues[2]);
-      stream.skip(3).then(resolvedSkippedValues => {
-        expect(resolvedSkippedValues).toMatchObject(skippedValues);
-        done();
-      });
+      const resolvedSkippedValues = await stream.skip(3);
+      expect(resolvedSkippedValues).toMatchObject(skippedValues);
     });
   });
-  it('should both next + skip operations', done => {
+  it('should both next + skip operations', async () => {
     const stream = createStream();
     const initialValue = 'a';
     const skippedValues = ['a', 'b', 'c'];
-    stream
-      .next()
-      .then(resolvedInitialValue => {
-        expect(resolvedInitialValue).toBe(initialValue);
-        stream(skippedValues[0]);
-        stream(skippedValues[1]);
-        stream(skippedValues[2]);
-        return stream.skip(3);
-      })
-      .then(resolvedSkippedValues => {
-        expect(resolvedSkippedValues).toMatchObject(skippedValues);
-        done();
-      });
+    const resolvedInitialValuePromise = stream.next();
     stream(initialValue);
+    const resolvedInitialValue = await resolvedInitialValuePromise;
+    expect(resolvedInitialValue).toBe(initialValue);
+    stream(skippedValues[0]);
+    stream(skippedValues[1]);
+    stream(skippedValues[2]);
+    const resolvedSkippedValues = await stream.skip(3);
+    expect(resolvedSkippedValues).toMatchObject(skippedValues);
   });
-  it('should both skip + next operations', done => {
+  it('should both skip + next operations', async () => {
     const stream = createStream();
     const initialValue = 'a';
     const skippedValues = ['a', 'b', 'c'];
-    stream
-      .next()
-      .then(resolvedInitialValue => {
-        expect(resolvedInitialValue).toBe(initialValue);
-        stream(skippedValues[0]);
-        stream(skippedValues[1]);
-        stream(skippedValues[2]);
-        return stream.skip(3);
-      })
-      .then(resolvedSkippedValues => {
-        expect(resolvedSkippedValues).toMatchObject(skippedValues);
-        done();
-      });
+    const resolvedInitialValuePromise = stream.next();
     stream(initialValue);
+    const resolvedInitialValue = await resolvedInitialValuePromise;
+    expect(resolvedInitialValue).toBe(initialValue);
+    stream(skippedValues[0]);
+    stream(skippedValues[1]);
+    stream(skippedValues[2]);
+    const resolvedSkippedValues = await stream.skip(3);
+    expect(resolvedSkippedValues).toMatchObject(skippedValues);
   });
 });
