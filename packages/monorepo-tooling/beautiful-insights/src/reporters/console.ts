ec464ddf9f55207c6bbe9b9fe643cecdb749991b
Marco de Jongh <mdejongh@atlassian.com>
2019-03-27 18:44:47 +1100

    Improve console logger

--- packages/monorepo-tooling/beautiful-insights/src/reporters/console.ts
+++ packages/monorepo-tooling/beautiful-insights/src/reporters/console.ts
@@ -1,15 +1,13 @@
 import chalk from 'chalk';
-import {
-  InsightsReport,
-  InsightsReportResults,
-} from '../reports/insights-report';
+import { InsightsReport, Severity } from '../reports/insights-report';
 
 export default (insightsReport: InsightsReport) => {
-  if (insightsReport.status === InsightsReportResults.FAIL) {
+  if (insightsReport.annotations.length > 0) {
     console.log(`Current branch introduces the following extra duplicates:`);
 
-    insightsReport.annotations.forEach(({ message, path, line }) => {
-      console.log(chalk.red(`${chalk.bold(`${path}:${line}`)}:  ${message}`));
+    insightsReport.annotations.forEach(({ message, path, line, severity }) => {
+      const color = severity === Severity.HIGH ? chalk.red : chalk.yellow;
+      console.log(color(`${chalk.bold(`${path}:${line}`)}:  ${message}`));
     });
   }
 };
