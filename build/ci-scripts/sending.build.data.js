bf1cc64aad142e09046605e7c9fcc4a0a34f8a42
raja <rbellebon@atlassian.com>
2019-03-28 10:21:51 +1100

    Update based on review from bbc pipelines

--- build/ci-scripts/sending.build.data.js
+++ build/ci-scripts/sending.build.data.js
@@ -1,14 +1,19 @@
-/* Script to send build data to datadog using Datadogs DogStatsD */
+/* Script to send build data to the fabric build stats service. */
 const getPipelinesBuildData = require('../utils/getBuildData')
   .getPipelinesBuildData;
 const sendData = require('../utils/sendPayload').sendData;
-const buildData = require('../../.build-data/data.json');
 
 (async () => {
   try {
-    console.log('buildId :', buildData.build_number);
-    console.log('buildStatus :', buildData.build_status);
-    sendData(buildData);
+    const buildId = process.env.BITBUCKET_BUILD_NUMBER;
+    console.log('buildId :', buildId);
+    console.log('buildStatus :', process.env.BITBUCKET_EXIT_CODE);
+    const buildData = await getPipelinesBuildData(
+      'atlassian',
+      'atlaskit-mk-2',
+      buildId,
+    );
+    await sendData(buildData);
   } catch (err) {
     console.error(`You face some issues while sending data: ${err}`);
     // It is not required to fail the step.
--- build/utils/sendPayload.js
+++ build/utils/sendPayload.js
@@ -1,9 +1,9 @@
 // @flow
 'use strict';
 /*
- * Utilities helper to send the data to Datadog.
+ * Utilities helper to send the data to the fabric build stats service.
  */
-const StatsD = require('hot-shots');
+const axios = require('axios');
 
 /*::
 type IStepsDataType = {
@@ -23,69 +23,26 @@ type IStepsDataType = {
   build_steps: Array<IStepsDataType>
 }
 */
+const fabricStatsServiceUrl =
+  process.env.FABRIC_STATS_SERVICE || 'http://localhost:8080/api/sendBuildData';
 
-function sendData(payload /*: IBuildEventProperties*/) {
-  const client = new StatsD({
-    // TODO: when it is working in dev, we need to target production.
-    // Check if we can add a prefix.
-    host: 'statsd.ap-southeast-2.dev.paas-inf.net',
-    port: 8125,
-    errorHandler(error /*: Error*/) {
-      console.log('Socket errors caught here: ', error);
+async function sendData(payload /*: IBuildEventProperties */) {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'cache-control': 'no-cache',
+      'shared-secret': process.env.SHARED_SECRET,
     },
-    // obs_sfx_enable enables to send data to signalFX
-    globalTags: {
-      env: process.env.NODE_ENV || 'Production',
-      obs_sfx_enable: 'true',
-    },
-  });
-
-  client.event(
-    'atlaskit_build_pipelines_data',
-    'The event related to pipelines data for Atlaskit',
+  };
+  const response = await axios.post(
+    `${fabricStatsServiceUrl}`,
+    payload,
+    config,
   );
-  console.log('Sending payload data from script', payload);
-  // Create increment metrics per build type: Default, Master, Landkid.
-  client.increment('build_count', { build_type: payload.build_type });
-
-  // Whenever there will be a build error, it will emit an event and increment a counter.
-  if (payload.build_status === 'FAILED') {
-    client.event('atlaskit_build_error', payload.build_name);
-    client.increment('build_error', { build_type: payload.build_type });
+  if (!response || !response.data || response.data.locked !== false) {
+    console.log('Something may have gone wrong...');
+    process.exit(1);
   }
-
-  client.gauge('build_time', payload.build_time, 1, {
-    build_type: payload.build_type,
-  });
-  client.gauge('build_number_steps', payload.build_number_steps, {
-    build_type: payload.build_type,
-  });
-
-  for (const step of payload.build_steps) {
-    if (step) {
-      client.gauge('build_time', step.step_duration, {
-        build_type: payload.build_type,
-        step_name: step.step_name,
-        step_status: step.step_status,
-      });
-      // Whenever there will be a step error, it will emit an event and increment a counter.
-      if (step.step_status === 'FAILED') {
-        client.event('atlaskit_step_error', step.step_name);
-        client.increment('build_error', {
-          build_type: payload.build_type,
-          step_name: step.step_name,
-        });
-      }
-    }
-  }
-
-  client.close(function(err /*: Error*/) {
-    if (err) {
-      console.log('The close did not work quite right: ', err);
-    } else {
-      console.log('The build data has been correctly sent!');
-    }
-  });
 }
 
 module.exports = { sendData };
