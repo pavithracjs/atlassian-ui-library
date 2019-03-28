09ade64142c9e3f5e58759c5c5966739727d3272
Kang Wei Chan <kchan@atlassian.com>
2019-03-28 14:47:33 +1100

    Update server-side test

--- packages/core/inline-edit/src/components/__tests__/unit/server-side.tsx
+++ packages/core/inline-edit/src/components/__tests__/unit/server-side.tsx
@@ -6,13 +6,12 @@ import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
 import * as ReactDOMServer from 'react-dom/server';
 
 test('Inline edit server side rendering', async () => {
-  const examples = await getExamplesFor('inline-edit');
-  const ex = examples[3].filePath;
-  // console.log(ex);
-  const Example = require(ex).default;
-  expect(() => ReactDOMServer.renderToString(<Example />)).not.toThrowError();
-  // await getExamplesFor('inline-edit'))  (await getExamplesFor('inline-edit')).forEach((examples: {filePath: string}) => {
-  //     const Example = require(examples.filePath).default;
-  //     expect(() => ReactDOMServer.renderToString(<Example />)).not.toThrowError();
-  //   });
+  (await getExamplesFor('inline-edit')).forEach(
+    (examples: { filePath: string }) => {
+      const Example = require(examples.filePath).default;
+      expect(() =>
+        ReactDOMServer.renderToString(<Example />),
+      ).not.toThrowError();
+    },
+  );
 });
