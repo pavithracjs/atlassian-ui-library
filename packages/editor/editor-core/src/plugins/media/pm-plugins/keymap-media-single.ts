086c958a6382f4ad7a60532b8d8a354e764b9577
Jesús Quintana <jquintana@atlassian.com>
2019-04-02 10:57:56 +1100

    ED-6319 Fix behaviour of media single wrap right

--- packages/editor/editor-core/src/plugins/media/pm-plugins/keymap-media-single.ts
+++ packages/editor/editor-core/src/plugins/media/pm-plugins/keymap-media-single.ts
@@ -1,9 +1,11 @@
 import { keymap } from 'prosemirror-keymap';
-import { Schema } from 'prosemirror-model';
+import { Schema, Node } from 'prosemirror-model';
 import { Plugin, Selection } from 'prosemirror-state';
 import * as keymaps from '../../../keymaps';
 import { isEmptyNode } from '../../../utils';
 import { Command } from '../../../types';
+import { safeInsert } from 'prosemirror-utils';
+import paragraph from '../../../../../renderer/src/email/nodes/paragraph';
 
 /**
  * When there's any empty block before another paragraph with wrap-right
@@ -23,7 +25,12 @@ const maybeRemoveMediaSingleNode = (schema: Schema): Command => {
   return (state, dispatch) => {
     const { selection } = state;
     // Check if we have a structure like
-    // anyBlock[empty] > mediaSingle[wrap-right] > [selection{empty, at start}]
+    // Doc/BlockNode:
+    //    anyBlock[empty] |
+    //    mediaSingle[wrap-right]
+    //    [selection{empty, at start}]
+
+    // Part 3,1: [selection{empty}]
     if (!selection.empty) {
       return false;
     }
@@ -32,10 +39,12 @@ const maybeRemoveMediaSingleNode = (schema: Schema): Command => {
     const { doc } = state;
     const index = $from.index($from.depth - 1);
 
+    // Part 3.2: [selection{empty, at start}]
     if ($from.parentOffset > 0) {
       return false;
     }
 
+    // Part 2: mediaSingle[wrap-right]
     const maybeMediaSingle = doc.maybeChild(index - 1);
     const isNotWrapRight =
       !maybeMediaSingle ||
@@ -46,30 +55,37 @@ const maybeRemoveMediaSingleNode = (schema: Schema): Command => {
     }
 
     // Here is wrap right
+    // Part 1: anyBlock[empty]
     const maybeAnyBlock = doc.maybeChild(index - 2);
-    if (!maybeAnyBlock || isEmptyNodeInSchema(maybeAnyBlock)) {
+    if (!maybeAnyBlock) {
       // the last is the image so should let the default behaviour delete the image
       return false;
     }
 
     // Should find the position
+    // TODO: Should move the current paragraph to the last line
+    const currentSelectionPos = $from.pos;
+    const mediaSinglePos = currentSelectionPos - maybeMediaSingle!.nodeSize;
+    const maybeAnyBlockPos = mediaSinglePos - maybeAnyBlock.nodeSize;
+    const { paragraph } = schema.nodes;
     let tr = state.tr;
-    doc.forEach((node, offset, currentIndex) => {
-      if (currentIndex === index - 2) {
-        tr = tr.replace(
-          offset + maybeAnyBlock.nodeSize - 2,
-          offset + maybeAnyBlock.nodeSize - 1,
-        );
-        tr.setSelection(
-          Selection.findFrom(
-            tr.doc.resolve(offset + maybeAnyBlock.nodeSize - 2),
-            -1,
-            true,
-          )!,
-        );
+    if (isEmptyNodeInSchema(maybeAnyBlock)) {
+      tr = tr.replace(
+        maybeAnyBlockPos - 1,
+        maybeAnyBlockPos + maybeAnyBlock.nodeSize,
+      );
+    } else {
+      tr.replace($from.pos, $from.pos + $from.parent.nodeSize - 1); // Remove content let empty paragraph;
+      if (maybeAnyBlock.type === paragraph) {
+        const insideParagraphPos =
+          maybeAnyBlockPos + maybeAnyBlock.nodeSize - 2;
+        safeInsert($from.parent.content, insideParagraphPos)(tr);
+      } else {
+        // Move content ups
+        const endOfBlockPos = maybeAnyBlockPos + maybeAnyBlock.nodeSize - 1;
+        safeInsert($from.parent!.copy($from.parent.content), endOfBlockPos)(tr);
       }
-      return;
-    });
+    }
 
     if (dispatch) {
       dispatch(tr);
