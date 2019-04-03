6b99b532314fcadad940de03ea860ff50d03fca8
Andrew Campbell <acampbell3@atlassian.com>
2019-04-03 17:02:59 +1100

    Changes broken button padding logic for Flag

--- packages/core/flag/src/theme.js
+++ packages/core/flag/src/theme.js
@@ -67,11 +67,11 @@ export const actionButtonStyles = (props: any) => ({
 });
 
 export const getPseudos = (p: any) => {
-  const padding = p.appearance === 'normal' ? gridSize() / 2 : gridSize;
+  const padding = p.appearance === 'normal' ? 0 : gridSize();
   return {
     '&, a&': {
       fontWeight: '500',
-      padding: `0 ${+padding}px !important`,
+      padding: `0 ${padding}px !important`,
     },
     '&:focus': {
       boxShadow: `0 0 0 2px ${flagFocusRingColor(p)}`,
