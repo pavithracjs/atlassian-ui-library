b50c289008cb638bf1ac0e190cf6396dbc844f9a
Nicholas Hiebl <nhiebl@atlassian.com>
2019-04-04 16:55:50 +1100

    AK-6072 Don't close popup select when cleared.

--- packages/core/select/src/PopupSelect/Select.js
+++ packages/core/select/src/PopupSelect/Select.js
@@ -164,7 +164,7 @@ export default class PopupSelect extends PureComponent<Props, State> {
   };
   handleSelectChange = (value: Object, actionMeta: Object) => {
     const { closeMenuOnSelect, onChange } = this.props;
-    if (closeMenuOnSelect) this.close();
+    if (closeMenuOnSelect && actionMeta.action !== 'clear') this.close();
     if (onChange) onChange(value, actionMeta);
   };
 
