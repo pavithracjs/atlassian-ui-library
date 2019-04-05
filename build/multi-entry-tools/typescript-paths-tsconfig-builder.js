806744f1db28c589df6daf6bcedede767f15c891
Marco de Jongh <mdejongh@atlassian.com>
2019-04-05 11:45:28 +1100

    Make tsconfig paths non-absolute

--- build/multi-entry-tools/typescript-paths-tsconfig-builder.js
+++ build/multi-entry-tools/typescript-paths-tsconfig-builder.js
@@ -3,12 +3,12 @@ const fromEntries = require('./utils/fromEntries');
 
 async function main() {
   const mapping = await getAlternativeEntryPointAliasMap();
+  const cwd = process.cwd();
   const paths = fromEntries(
     Object.entries(mapping).map(([moduleName, path]) => {
-      return [[moduleName], [path]];
+      return [[moduleName], [path.replace(cwd, '../..')]];
     }),
   );
-
   console.log(
     JSON.stringify(
       {
