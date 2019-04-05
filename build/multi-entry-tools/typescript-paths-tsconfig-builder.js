963021f12baf598d84964c89121cf012fec2bc69
Marco de Jongh <mdejongh@atlassian.com>
2019-04-05 11:53:38 +1100

    Fix up paths and filter website and build

--- build/multi-entry-tools/typescript-paths-tsconfig-builder.js
+++ build/multi-entry-tools/typescript-paths-tsconfig-builder.js
@@ -5,10 +5,19 @@ async function main() {
   const mapping = await getAlternativeEntryPointAliasMap();
   const cwd = process.cwd();
   const paths = fromEntries(
-    Object.entries(mapping).map(([moduleName, path]) => {
-      return [[moduleName], [path.replace(cwd, '../..')]];
-    }),
+    Object.entries(mapping)
+      .filter(([, path]) => path.includes('/packages/'))
+      .map(([moduleName, path]) => {
+        return [[moduleName], [path.replace(`${cwd}/packages`, '../..')]];
+      }),
   );
+  console.log(
+    '/* This file is auto-generated to get multi entry points to type check correctly */',
+  );
+  console.log(
+    '/* When you add a new entry point in src/ rebuild it buy running:  bolt build:multi-entry-point-tsconfig */ */',
+  );
+
   console.log(
     JSON.stringify(
       {
