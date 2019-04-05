7a3dc5aa73b87179adf13d37736fdc2452eb3986
Ishan Somasiri <isomasiri@atlassian.com>
2019-04-05 12:22:36 +1100

    TEAMS-396 : Changed string logic of byline

--- packages/elements/mention/src/__tests__/unit/components/MentionDescriptionHighlight/TeamMentionDescriptionHighlightSpec.tsx
+++ packages/elements/mention/src/__tests__/unit/components/MentionDescriptionHighlight/TeamMentionDescriptionHighlightSpec.tsx
@@ -1,44 +1,54 @@
 import { render } from 'enzyme';
 import * as React from 'react';
 import TeamMentionDescriptionByline from '../../../../components/MentionDescriptionByline';
+import { MentionDescription } from '../../../..//types';
 import { teamMention } from './_commonData';
 
-const shallowRender = (memberCount: number, includesYou: boolean) => {
-  const updatedMention = {
-    ...teamMention,
-    context: {
-      members: [
-        {
-          id: 'user-1234',
-          name: 'Test User',
-        },
-      ],
-      ...teamMention.context,
-      includesYou: includesYou,
-      memberCount: memberCount,
-    },
-  };
-  return render(<TeamMentionDescriptionByline mention={updatedMention} />);
+const renderByline = (teamData: MentionDescription) => {
+  return render(<TeamMentionDescriptionByline mention={teamData} />);
 };
+
+const buildTeamData = (memberCount: number, includesYou: boolean) => ({
+  ...teamMention,
+  context: {
+    members: [
+      {
+        id: 'user-1234',
+        name: 'Test User',
+      },
+    ],
+    ...teamMention.context,
+    includesYou: includesYou,
+    memberCount: memberCount,
+  },
+});
 
 describe('Team mention description', () => {
   it('should render Team Mention description component with less than 50 members, includes you', () => {
-    const component = shallowRender(5, true);
+    const component = renderByline(buildTeamData(5, true));
     expect(component).toMatchSnapshot();
   });
 
   it('should render Team Mention description component with more than 50 members, includes you', () => {
-    const component = shallowRender(55, true);
+    const component = renderByline(buildTeamData(55, true));
     expect(component).toMatchSnapshot();
   });
 
   it('should render Team Mention description component with less than 50 members, not including you', () => {
-    const component = shallowRender(4, false);
+    const component = renderByline(buildTeamData(4, false));
     expect(component).toMatchSnapshot();
   });
 
   it('should render Team Mention description component with more than 50 members, not including you', () => {
-    const component = shallowRender(100, false);
+    const component = renderByline(buildTeamData(100, false));
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should not render Team Mention description component if context is not given', () => {
+    const newMention = { ...teamMention };
+    delete newMention.context;
+
+    const component = renderByline(newMention);
     expect(component).toMatchSnapshot();
   });
 });
