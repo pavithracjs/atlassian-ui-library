f197f4055a807c1e5599b9a2bb65ca3925116bf6
raja <rbellebon@atlassian.com>
2019-04-08 11:43:29 +1000

    Changes to webpack to run the website and add vr tests for the website

--- website/src/__tests__/visual-regression/home.ts
+++ website/src/__tests__/visual-regression/home.ts
@@ -1,10 +1,38 @@
-import { takeScreenShot } from '@atlaskit/visual-regression/helper';
+import { takeElementScreenShot } from '@atlaskit/visual-regression/helper';
+
+const app = '#app';
+const defaultView = '[spacing="cosy"]';
+const atlaskitLogo = '[alt="Atlaskit logo"]';
+const atlaskitTitle = 'h1';
 
 describe('Snapshot Test', () => {
-  it('Home page should match snapshot', async () => {
-    const url = 'http://localhost:9000/';
+  let page: any;
+  let url: string;
+
+  beforeAll(async () => {
+    url = 'http://localhost:9000';
     // @ts-ignore
-    const image = await takeScreenShot(global.page, url);
+    page = global.page;
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.waitForSelector(app);
+    await page.setViewport({ width: 1800, height: 1000 });
+    await page.waitForSelector(defaultView);
+  });
+
+  it('Home page title should match production', async () => {
+    const image = await takeElementScreenShot(page, atlaskitTitle);
     expect(image).toMatchProdImageSnapshot();
+  });
+  it('Home page logo should match production', async () => {
+    const image = await takeElementScreenShot(page, atlaskitLogo);
+    expect(image).toMatchProdImageSnapshot();
+  });
+  it('Home page cards should match production', async () => {
+    const cards = await page.$$('a');
+    // The element 0 is the logo of Atlaskit, already covered by the test above.
+    for (let i = 1; i < cards.length; i++) {
+      let image = await cards[i].screenshot();
+      expect(image).toMatchProdImageSnapshot();
+    }
   });
 });
