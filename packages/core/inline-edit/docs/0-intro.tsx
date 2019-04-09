8915078a06c3115332c9b13f38c6ff00b0548f39
Kang Wei Chan <kchan@atlassian.com>
2019-04-09 11:46:00 +1000

    Rename textfield docs page

--- packages/core/inline-edit/docs/0-intro.tsx
+++ packages/core/inline-edit/docs/0-intro.tsx
@@ -9,7 +9,7 @@ export default md`
   The default exported InlineEdit component is a standalone component (not meant to be used
   within Form), requiring a custom read view and edit view to be passed in as props. If you
   would like a simple inline edit with a text input, you may be able to use the
-  [InlineEditableTextfield component](/packages/core/inline-edit/docs/textfield).
+  [InlineEditableTextfield component](/packages/core/inline-edit/docs/inline-editable-textfield).
 
   If you are seeking guidance on how to upgrade the inline edit component from a previous
   version, see [this upgrade guide](/packages/core/inline-edit/docs/upgrade-guide).
