9e2378bfe24c135e868c711181070eb043a27f41
Kang Wei Chan <kchan@atlassian.com>
2019-04-09 13:45:09 +1000

    Fix readviewcontainer height

--- packages/core/inline-edit/src/styled/ReadViewContainer.tsx
+++ packages/core/inline-edit/src/styled/ReadViewContainer.tsx
@@ -9,12 +9,11 @@ const ReadViewContainer = styled.div<Props>`
   display: flex;
   max-width: 100%;
   overflow: hidden;
-  padding: 8px 6px;
+  padding: ${props => (props.isCompact ? gridSize() / 2 : gridSize())}px
+    ${gridSize() - 2}px;
   font-size: ${fontSize()}px;
-  height: ${props =>
-    (gridSize() * (props.isCompact ? 1.5 : 2.5)) / fontSize()}em;
-  line-height: ${props =>
-    (gridSize() * (props.isCompact ? 1.5 : 2.5)) / fontSize()};
+  height: ${(gridSize() * 2.5) / fontSize()}em;
+  line-height: ${(gridSize() * 2.5) / fontSize()};
 `;
 
 ReadViewContainer.displayName = 'ReadViewContainer';
