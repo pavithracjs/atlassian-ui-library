f176eb7d07e56a75f7e698e4b2c54f8404b217a5
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2019-04-11 13:29:36 +1000

    Fix Media Header Download Button test

--- packages/media/media-viewer/src/__tests__/unit/newgen/header.spec.tsx
+++ packages/media/media-viewer/src/__tests__/unit/newgen/header.spec.tsx
@@ -296,7 +296,7 @@ describe('<Header />', () => {
       el: ReactWrapper<any, any>,
       enabled: boolean,
     ) => {
-      expect(el.find({ isDisabled: !enabled }).find(Button)).toHaveLength(1);
+      expect(el.find({ isDisabled: !enabled }).find('button')).toHaveLength(1);
       expect(el.find(DownloadIcon)).toHaveLength(1);
     };
 
