f882a514fd17e85a0e3902cda3eb687565e20a68
Mauro Alvarez <contact@mauroalvarez.com>
2019-04-25 12:36:34 -0300

    global-help - help-article - Update example

--- packages/help/help-article/docs/0-intro.tsx
+++ packages/help/help-article/docs/0-intro.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
 import { md, code, Example, Props } from '@atlaskit/docs';
+import SectionMessage from '@atlaskit/section-message';
 
 export default md`
+
+${(
+  <SectionMessage appearance="warning">
+    <p>
+      <strong>
+        Note: @atlaskit/help-panel is currently a developer preview.
+      </strong>
+    </p>
+    <p>
+      Please experiment with and test this package, but be aware that the API
+      may change at any time. Use at your own risk, preferrably not in
+      production.
+    </p>
+  </SectionMessage>
+)}
+
+
   ## Usage
 
   ${code`
