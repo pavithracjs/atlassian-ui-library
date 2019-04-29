768b0fde350157a63ac622a4f61a7ae41c410b44
Patrick Lai <plai@atlassian.com>
2019-04-29 16:03:11 +1000

    revert: unskip drawer ssr test that was skipped in 68b99e3

--- packages/core/drawer/src/components/__tests__/unit/server-side-hydrate.js
+++ packages/core/drawer/src/components/__tests__/unit/server-side-hydrate.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
 import { ssr } from '@atlaskit/ssr';
 
-jest.spyOn(global.console, 'error').mockImplementation(() => {});
+jest.spyOn(global.console, 'error');
 
 afterEach(() => {
   jest.resetAllMocks();
@@ -15,7 +15,7 @@ afterEach(() => {
  * Ticket: https://ecosystem.atlassian.net/browse/AK-6059
  */
 /* eslint-disable jest/no-disabled-tests */
-test.skip('should ssr then hydrate drawer correctly', async () => {
+test('should ssr then hydrate drawer correctly', async () => {
   const [example] = await getExamplesFor('drawer');
   // $StringLitteral
   const Example = require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
@@ -24,5 +24,12 @@ test.skip('should ssr then hydrate drawer correctly', async () => {
   elem.innerHTML = await ssr(example.filePath);
 
   ReactDOM.hydrate(<Example />, elem);
-  expect(console.error).not.toBeCalled(); // eslint-disable-line no-console
+
+  // ignore emotion errors in server
+  const ignorePattern = /Did not expect server HTML to contain a <style> in <div>./;
+  const mockCalls = console.error.mock.calls.filter(
+    e => !e[0].match(ignorePattern),
+  );
+
+  expect(mockCalls.length).toBe(0); // eslint-disable-line no-console
 });
