e16888d46562e1e97899dc4418701de6e346d611
Fred Ghilini <fghilini@atlassian.com>
2019-04-29 12:09:47 +1000

    ED-6486: refactored unit test

--- packages/editor/editor-mobile-bridge/src/__tests__/unit/editor/native-to-web/bridge-test.ts
+++ packages/editor/editor-mobile-bridge/src/__tests__/unit/editor/native-to-web/bridge-test.ts
@@ -12,8 +12,11 @@ const mockEditorCore = {
   setLinkText: jest.fn(() => () => mockCalls.push('setLinkText')),
 };
 jest.mock('@atlaskit/editor-core', () => mockEditorCore);
+jest.mock('../../../../version.json', () => ({
+  name: '@atlaskit/editor-mobile-bridge',
+  version: '1.2.3.4',
+}));
 
-import semver from 'semver';
 import WebBridgeImpl from '../../../../editor/native-to-web';
 import {
   indentList,
@@ -26,6 +29,7 @@ import {
   setLinkHref,
   setLinkText,
 } from '@atlaskit/editor-core';
+import { version as packageVersion } from '../../../../version.json';
 
 afterEach(() => {
   (indentList as jest.Mock<{}>).mockClear();
@@ -43,7 +47,7 @@ describe('general', () => {
   let bridge: any = new WebBridgeImpl();
 
   it('should return valid bridge version', () => {
-    expect(semver.valid(bridge.currentVersion())).toBeTruthy();
+    expect(bridge.currentVersion()).toEqual(packageVersion);
   });
 });
 
