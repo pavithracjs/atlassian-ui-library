39a33f8b0b6ba177da8f13bed4f28142eaed77d2
Mauro Alvarez <contact@mauroalvarez.com>
2019-04-30 14:55:14 -0300

    gobal-help - help-panel - make title and body props mandatories

--- packages/help/help-panel/src/components/Article/ArticleContent.tsx
+++ packages/help/help-panel/src/components/Article/ArticleContent.tsx
@@ -2,22 +2,20 @@ import * as React from 'react';
 import { ArticleContentInner, ArticleContentTitle } from './styled';
 
 export interface Props {
-  title?: string;
-  body?: string;
+  title: string;
+  body: string;
 }
 
 const ArticleContent: React.SFC<Props> = props => {
   const { title = '', body = '' } = props;
   return (
     <ArticleContentInner>
-      {title && title !== undefined && (
+      {title !== '' && (
         <ArticleContentTitle>
           <h2>{title}</h2>
         </ArticleContentTitle>
       )}
-      {body && body !== undefined && (
-        <div dangerouslySetInnerHTML={{ __html: body }} />
-      )}
+      {body !== '' && <div dangerouslySetInnerHTML={{ __html: body }} />}
     </ArticleContentInner>
   );
 };
