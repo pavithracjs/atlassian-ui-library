6a2226fecb0688fd2556402883b348269bd5ab72
Marco de Jongh <mdejongh@atlassian.com>
2019-05-01 12:59:15 +1000

    Add more documentation

--- build/utils/sanity-check-file-structure.js
+++ build/utils/sanity-check-file-structure.js
@@ -1,3 +1,22 @@
+/*
+TypeScript compiler has this weird problem where it sometimes decides to also build
+imported code. When this happens the code we want to distribute actually ends up in a sub-dir instead of
+the root of the package dir.
+
+For example in editor-core we once ended up with the following structure:
+- editor/editor-core/editor-core
+- editor/editor-core/elements
+- etc...
+
+Where-as expected would be:
+- editor/editor-core/index.js
+- editor/editor-core/components/*
+- etc....
+
+This check does a quick compare of the src/ directory with the root of the package dir.
+So this check needs to be run *AFTER* the dists are build. This should help us prevent shipping broken code.
+
+*/
 const { readFile, readdir } = require('./fs');
 
 /*
@@ -13,6 +32,11 @@ const main = async () => {
     readFile('./package.json').then(res => JSON.parse(res)),
   ]);
 
+  /*
+  This check is not relevant for packages that still ship ES5 and ES2015.
+  it's also not relevant for private packages because those are allmost all test helpers
+  that do funky stuff with their dist anyway (and are never published)
+  */
   if (packageJSON.module !== 'index.js' || packageJSON.private) {
     return;
   }
