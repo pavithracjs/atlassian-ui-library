24e46e7268a5f4594c4426464d5edad6856e6903
Marco de Jongh <mdejongh@atlassian.com>
2019-05-01 10:23:15 +1000

    Add some exceptions

--- build/utils/sanity-check-file-structure.js
+++ build/utils/sanity-check-file-structure.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const src = fs.readdirSync('./src');
 const root = fs.readdirSync('./');
 
-const missing = src.filter(fileName => !root.includes(fileName));
+const missing = src
+  .filter(fileName => fileName !== '__tests__')
+  .filter(fileName => !root.includes(fileName.replace(/\.tsx?/, '.js')));
 if (missing.length > 0) {
   throw new Error(
     `Build files in root are  missing some files: ${missing.join(',')}`,
