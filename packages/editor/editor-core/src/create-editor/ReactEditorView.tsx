9df5dfd03e126c1111fde4a84b36710d8450f7c5
Ivaturi Sriharsha <isriharsha@atlassian.com>
2019-05-03 06:51:55 +0000

    Undo unintended changes due to a botched merge from master

--- packages/editor/editor-core/src/create-editor/ReactEditorView.tsx
+++ packages/editor/editor-core/src/create-editor/ReactEditorView.tsx
@@ -57,26 +57,32 @@ export interface EditorViewProps {
   providerFactory: ProviderFactory;
   portalProviderAPI: PortalProviderAPI;
   allowAnalyticsGASV3?: boolean;
-  render?: (props: {
-    editor: JSX.Element;
-    view?: EditorView;
-    config: EditorConfig;
-    eventDispatcher: EventDispatcher;
-    transformer?: Transformer<string>;
-    dispatchAnalyticsEvent: DispatchAnalyticsEvent;
-  }) => JSX.Element;
-  onEditorCreated: (instance: {
-    view: EditorView;
-    config: EditorConfig;
-    eventDispatcher: EventDispatcher;
-    transformer?: Transformer<string>;
-  }) => void;
-  onEditorDestroyed: (instance: {
-    view: EditorView;
-    config: EditorConfig;
-    eventDispatcher: EventDispatcher;
-    transformer?: Transformer<string>;
-  }) => void;
+  render?: (
+    props: {
+      editor: JSX.Element;
+      view?: EditorView;
+      config: EditorConfig;
+      eventDispatcher: EventDispatcher;
+      transformer?: Transformer<string>;
+      dispatchAnalyticsEvent: DispatchAnalyticsEvent;
+    },
+  ) => JSX.Element;
+  onEditorCreated: (
+    instance: {
+      view: EditorView;
+      config: EditorConfig;
+      eventDispatcher: EventDispatcher;
+      transformer?: Transformer<string>;
+    },
+  ) => void;
+  onEditorDestroyed: (
+    instance: {
+      view: EditorView;
+      config: EditorConfig;
+      eventDispatcher: EventDispatcher;
+      transformer?: Transformer<string>;
+    },
+  ) => void;
 }
 
 export default class ReactEditorView<T = {}> extends React.Component<
@@ -89,10 +95,9 @@ export default class ReactEditorView<T = {}> extends React.Component<
   editorState: EditorState;
   errorReporter: ErrorReporter;
   dispatch: Dispatch;
-  analyticsEventHandler: (payloadChannel: {
-    payload: AnalyticsEventPayload;
-    channel?: string;
-  }) => void;
+  analyticsEventHandler: (
+    payloadChannel: { payload: AnalyticsEventPayload; channel?: string },
+  ) => void;
 
   static contextTypes = {
     getAtlaskitAnalyticsEventHandlers: PropTypes.func,
