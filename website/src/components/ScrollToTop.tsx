7f99a17ff7a20c850bce07147f0455ab7237272a
Ajay Narain Mathur <amathur@atlassian.com>
2019-05-06 18:24:03 +1000

    Added mobile navigation to the website

---
+++ website/src/components/ScrollToTop.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router';
+
+type ScrollToTopProps = RouteComponentProps<any>;
+
+class ScrollToTop extends React.Component<ScrollToTopProps> {
+  componentDidUpdate(prevProps: ScrollToTopProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default withRouter(ScrollToTop);
