a19a88b182a0f91fef2edfef87fb1d7fcffba730
Ahmed Fouad <afouad@atlassian.com>
2019-05-07 11:50:46 +1000

    QS-675 use gridsize instead of hard coding the number of pixels

--- packages/search/global-search/src/components/jira/JiraAdvancedSearch.tsx
+++ packages/search/global-search/src/components/jira/JiraAdvancedSearch.tsx
@@ -28,20 +28,20 @@ interface State {
 
 const TextContainer = styled.div`
   margin-right: ${gridSize()}px;
-  height: 24px;
-  line-height: 24px;
+  height: ${3 * gridSize()}px;
+  line-height: ${3 * gridSize()}px;
   white-space: nowrap;
 `;
 
 const Container = styled.div`
-  margin: 12px;
+  margin: ${1.5 * gridSize()}px;
   display: flex;
   flex-direction: row;
   justify-content: left;
 `;
 
 const ButtonWrapper = styled.div`
-  margin-right: 4px;
+  margin-right: ${0.5 * gridSize()}px;
 `;
 const itemI18nKeySuffix = [
   JiraEntityTypes.Issues,
@@ -55,13 +55,7 @@ const getI18nItemName = (i18nKeySuffix: string) => {
   const id = `jira_advanced_search_${i18nKeySuffix}` as keyof typeof messages;
   return <FormattedMessage {...messages[id]} />;
 };
-/**
- * Questions:
- * 1- check with mark if no issues and we have project/boards results what will happen to the advanced link
- * 2- move the top recent link as the bottom advanced link
- * 3- check if we need the callback or link component is enough
- * 4- analytics query version is missing
- */
+
 export default class JiraAdvancedSearch extends React.Component<Props, State> {
   state = {
     entity: JiraEntityTypes.Issues,
