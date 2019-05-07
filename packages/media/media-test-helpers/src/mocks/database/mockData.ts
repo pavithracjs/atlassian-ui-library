f6b857fe6aef3862edb3f25c61d8d06ca12799f8
Vladimir Vlasov <vvlasov@atlassian.com>
2019-05-07 16:26:33 +1000

    chore: fix default stub image

--- packages/media/media-test-helpers/src/mocks/database/mockData.ts
+++ packages/media/media-test-helpers/src/mocks/database/mockData.ts
@@ -33,7 +33,7 @@ export const getFakeFileName = (
 // This fake image was generated using faker. It never changes so we are just
 // using the output exact call Faker.image.dataUri(320, 240)
 export const fakeImage =
-  'data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22320%22%20height%3D%22240%22%3E%20%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%20%20%3Ctext%20x%3D%220%22%20y%3D%2220%22%20font-size%3D%2220%22%20text-anchor%3D%22start%22%20fill%3D%22white%22%3E320x240%3C%2Ftext%3E%20%3C%2Fsvg%3E';
+  'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgYmFzZVByb2ZpbGU9ImZ1bGwiIHdpZHRoPSIzMjAiIGhlaWdodD0iMjQwIj4gPHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0iZ3JleSIvPiAgPHRleHQgeD0iMCIgeT0iMjAiIGZvbnQtc2l6ZT0iMjAiIHRleHQtYW5jaG9yPSJzdGFydCIgZmlsbD0id2hpdGUiPjMyMHgyNDA8L3RleHQ+IDwvc3ZnPg==';
 
 export const getDateWithOffset = (offset: number) => {
   let time = new Date();
