f7c226479a38ae109dc42f30f5f04be4b4ee537e
Vladimir Vlasov <vvlasov@atlassian.com>
2019-05-09 17:28:09 +1000

    test: fix SSR test MORE

--- packages/media/media-viewer/examples/5-mocked-viewer.tsx
+++ packages/media/media-viewer/examples/5-mocked-viewer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { canUseDOM } from 'exenv';
 
+import { FileIdentifier, ContextFactory } from '@atlaskit/media-core';
+import { MediaFile } from '@atlaskit/media-store';
 import {
   MediaMock,
   defaultCollectionName,
@@ -9,10 +11,11 @@ import {
   defaultBaseUrl,
   generateFilesFromTestData,
 } from '@atlaskit/media-test-helpers';
-import { FileIdentifier, ContextFactory } from '@atlaskit/media-core';
 
 import { wideImage } from '../example-helpers/assets/wide-image';
 import { MediaViewer } from '../src/components/media-viewer';
+
+let files: Array<MediaFile & { blob: Blob }> = [];
 
 if (canUseDOM) {
   (window as any).areControlsRendered = () => {
@@ -27,26 +30,26 @@ if (canUseDOM) {
       return window.getComputedStyle(controls).opacity === '1';
     }
   };
-}
 
-const files = generateFilesFromTestData([
-  {
-    name: 'media-test-file-1.png',
-    dataUri: smallImage,
-  },
-  {
-    name: 'media-test-file-2.jpg',
-    dataUri: wideImage,
-  },
-  {
-    name: 'media-test-file-3.png',
-    dataUri: tallImage,
-  },
-]);
-const mediaMock = new MediaMock({
-  [defaultCollectionName]: files,
-});
-mediaMock.enable();
+  files = generateFilesFromTestData([
+    {
+      name: 'media-test-file-1.png',
+      dataUri: smallImage,
+    },
+    {
+      name: 'media-test-file-2.jpg',
+      dataUri: wideImage,
+    },
+    {
+      name: 'media-test-file-3.png',
+      dataUri: tallImage,
+    },
+  ]);
+  const mediaMock = new MediaMock({
+    [defaultCollectionName]: files,
+  });
+  mediaMock.enable();
+}
 
 const context = ContextFactory.create({
   authProvider: () =>
@@ -58,6 +61,9 @@ const context = ContextFactory.create({
 });
 export default class Example extends React.Component<{}, {}> {
   render() {
+    if (files.length === 0) {
+      return null;
+    }
     return (
       <MediaViewer
         dataSource={{
