7d8f3e4f4745c21e94d527f65a8cfd09f45b22e1
Mauro Alvarez <contact@mauroalvarez.com>
2019-05-09 17:29:51 -0300

    help-article - add prop titleLinkUrl to make the title of the article a link and update bundle-size-ratchet

--- packages/help/help-article/src/components/HelpArticle.tsx
+++ packages/help/help-article/src/components/HelpArticle.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
-import { ArticleContentInner, ArticleContentTitle } from './styled';
+import {
+  ArticleContentInner,
+  ArticleContentTitle,
+  ArticleContentTitleLink,
+} from './styled';
 import StyleTag from './styles/StyleTag';
+import ShortcutIcon from '@atlaskit/icon/glyph/shortcut';
 export interface Props {
   title?: string;
   body?: string;
+  titleLinkUrl?: string;
 }
 
 const HelpArticle: React.SFC<Props> = props => {
-  const { title = '', body = '' } = props;
+  const { title = '', body = '', titleLinkUrl } = props;
   return (
     <ArticleContentInner>
       <StyleTag />
       {title && (
         <ArticleContentTitle>
-          <h2>{title}</h2>
+          {titleLinkUrl ? (
+            <ArticleContentTitleLink href={titleLinkUrl}>
+              <h2>
+                {title}
+                <span> </span>
+                <ShortcutIcon label="link icon" size="small" />
+              </h2>
+            </ArticleContentTitleLink>
+          ) : (
+            <h2>{title}</h2>
+          )}
         </ArticleContentTitle>
       )}
       {body && (
