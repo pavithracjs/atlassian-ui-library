bd8f383b391f85b714471edd49bf5490cb2b2588
Tim Keir <tkeir@atlassian.com>
2019-05-09 21:26:26 +1000

    POC: Inline media by parsing ADF

--- packages/editor/renderer/src/__tests__/__helpers/page-objects/_media.ts
+++ packages/editor/renderer/src/__tests__/__helpers/page-objects/_media.ts
@@ -1,3 +1,41 @@
+import { traverse } from '@atlaskit/adf-utils/traverse';
+
+/*
 export const selectors = {
   errorLoading: '.media-single .overlay.error',
 };
+*/
+
+// 1px grey image
+const greyImage =
+  'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==';
+// const blackImage = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+
+/**
+ * Cleanse ADF Media Nodes
+ *
+ * Converts media nodes to an inlined black image.
+ *
+ * This allows us to bypass a network request to the Media API,
+ * which increases reliability in CI, and avoids mismatches via
+ * authenticaton or incorrect identifiers (which ar environmental).
+ *
+ * @param adf An parsed ADF document
+ */
+export function cleanseAdfMedia(adf: any) {
+  // Deep copy clone just in case the original state is needed later
+  adf = JSON.parse(JSON.stringify(adf));
+  const cleansedAdf = traverse(adf, {
+    media: (node: any) => {
+      const { attrs } = node;
+      if (node.type === 'media' && attrs.type !== 'external') {
+        // Instead of looking up the url by resolving the ID at runtime
+        // we set an external url for immediate access.
+        attrs.type = 'external';
+        attrs.url = greyImage;
+      }
+      return node;
+    },
+  });
+  return cleansedAdf;
+}
--- packages/editor/renderer/src/__tests__/visual-regression/media/renderer-media.ts
+++ packages/editor/renderer/src/__tests__/visual-regression/media/renderer-media.ts
@@ -6,9 +6,11 @@ import {
   mountRenderer,
 } from '../_utils';
 import adf from './__fixtures__/renderer-media.adf.json';
-import { selectors as mediaSelectors } from '../../__helpers/page-objects/_media';
+// import { selectors as mediaSelectors } from '../../__helpers/page-objects/_media';
 import { selectors as rendererSelectors } from '../../__helpers/page-objects/_renderer';
 import { Page } from 'puppeteer';
+import { cleanseAdfMedia } from '../../__helpers/page-objects/_media';
+import { waitForLoadedImageElements } from '@atlaskit/visual-regression/helper';
 
 const devices = [
   Device.LaptopHiDPI,
@@ -32,15 +34,18 @@ describe('Snapshot Test: Media', () => {
       devices.forEach(device => {
         it(`should correctly render ${device}`, async () => {
           await page.setViewport(deviceViewPorts[device]);
+          const _adf = cleanseAdfMedia(adf);
           await mountRenderer(page, {
             appearance: 'full-page',
             allowDynamicTextSizing: true,
-            document: adf,
+            // document: adf,
+            document: _adf,
           });
 
-          await page.waitForSelector(mediaSelectors.errorLoading); // In test should show overlay error
+          // await page.waitForSelector(mediaSelectors.errorLoading); // In test should show overlay error
 
           await page.waitForSelector(rendererSelectors.document);
+          await waitForLoadedImageElements(page, 1000, 0); // 1 second timeout and no media API delay.
           await snapshot(page, 0.01, rendererSelectors.document);
         });
       });
