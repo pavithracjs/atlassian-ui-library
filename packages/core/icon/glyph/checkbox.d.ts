dad119594151afcdb1263122e8e99a16193518d3
Patrick Lai <plai@atlassian.com>
2019-05-10 11:52:23 +1000

    update checkbox icon glyph from type to interface

--- packages/core/icon/glyph/checkbox.d.ts
+++ packages/core/icon/glyph/checkbox.d.ts
@@ -1,7 +1,7 @@
 import { PureComponent } from 'react';
 
-type State = {};
-type Props = {
+interface State {};
+interface Props {
   label: string;
   size?: 'small' | 'medium' | 'large' | 'xlarge';
   onClick?: (e: MouseEvent) => void;
