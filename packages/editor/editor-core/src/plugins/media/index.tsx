cab70241bd70cb7a77254cf3a3c6c950373495d6
Stanislav Sysoev <ssysoev@atlassian.com>
2019-05-10 11:10:19 +1000

    Fix re-rendering all typeahead items on hover/active item change

--- packages/editor/editor-core/src/plugins/media/index.tsx
+++ packages/editor/editor-core/src/plugins/media/index.tsx
@@ -161,7 +161,7 @@ const mediaPlugin = (
         title: formatMessage(messages.filesAndImages),
         description: formatMessage(messages.filesAndImagesDescription),
         priority: 400,
-        keywords: ['media'],
+        keywords: ['media', 'attachment'],
         icon: () => (
           <IconImages label={formatMessage(messages.filesAndImages)} />
         ),
