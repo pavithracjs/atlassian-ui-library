f680ed68127f55c846222a5216c4c504ab64c3da
Mauro Alvarez <contact@mauroalvarez.com>
2019-05-13 00:56:52 -0300

    help-panel - analytics - Read the name and version of the package from version.json

--- packages/help/help-panel/src/analytics.ts
+++ packages/help/help-panel/src/analytics.ts
@@ -2,7 +2,7 @@ import * as x from '@atlaskit/analytics-next';
 import {
   name as packageName,
   version as packageVersion,
-} from '../package.json';
+} from '../version.json';
 
 export const withAnalyticsEvents = x.withAnalyticsEvents;
 export const withAnalyticsContext = x.withAnalyticsContext;
