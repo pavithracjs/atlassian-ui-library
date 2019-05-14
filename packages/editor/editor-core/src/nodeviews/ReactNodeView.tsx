pavithracjs/atlassian-ui-library

commit 6734f41241f5e72dc455a7ec92dbe85092513286

detect if node has moved to see if we should re-render

packages/editor/editor-core/src/nodeviews/ReactNodeView.tsx
@@ -297,9 +297,12 @@ export class SelectionBasedNodeView extends ReactNodeView {
     return this.pos < from && to < this.posEnd;
   };
 
-  viewShouldUpdate(nextNode: PMNode) {
-    this.updatePos();
+  insideSelection = () => {
+    const { from, to } = this.view.state.selection;
+    return this.isSelectionInsideNode(from, to);
+  };
 
+  viewShouldUpdate(nextNode: PMNode) {
     const {
       state: { selection },
     } = this.view;
@@ -307,6 +310,15 @@ export class SelectionBasedNodeView extends ReactNodeView {
     // update selection
     const oldSelection = this.oldSelection;
     this.oldSelection = selection;
+
+    // update cached positions
+    const { pos: oldPos } = this;
+    this.updatePos();
+
+    if (oldPos !== this.pos) {
+      // node has moved position
+      return true;
+    }
 
     const { from, to } = selection;
     const { from: oldFrom, to: oldTo } = oldSelection;

packages/editor/editor-core/src/__tests__/visual-regression/table/__image_snapshots__/insert-cells-ts-snapshot-test-table-insert-delete-inserts-multiple-rows-in-succession-1-snap.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5710e15249a8dc52b5fd57e46a307024ac48db54e0903a08c35fa526146e8074
+size 23591

packages/editor/editor-core/src/__tests__/visual-regression/table/insert-cells.ts
@@ -52,6 +52,12 @@ describe('Snapshot Test: table insert/delete', () => {
     await insertRow(page, 1);
   });
 
+  it(`inserts multiple rows in succession`, async () => {
+    await insertRow(page, 1);
+    await insertRow(page, 1);
+    await insertRow(page, 1);
+  });
+
   // TODO: move this to integration tests in future
   it(`should be able to insert column`, async () => {
     await insertColumn(page, 1);

packages/editor/editor-core/src/plugins/table/nodeviews/cell.tsx
@@ -152,15 +152,11 @@ class CellView extends SelectionBasedNodeView {
     const disabledState: EditorDisabledPluginState = editorDisabledPluginKey.getState(
       this.view.state,
     );
+
     return (
       <CellComponent
         forwardRef={forwardRef}
-        withCursor={
-          this.isSelectionInsideNode(
-            this.view.state.selection.from,
-            this.view.state.selection.to,
-          ) && !!tableState.editorHasFocus
-        }
+        withCursor={this.insideSelection() && !!tableState.editorHasFocus}
         isResizing={!!tableResizingState && !!tableResizingState.dragging}
         isContextualMenuOpen={!!tableState.isContextualMenuOpen}
         isContextMenuEnabled={props.isContextMenuEnabled}

commit 932730d7eeb2d300c0023652177d8807e7948a68

fix performance regression after 6734f41

packages/editor/editor-core/src/nodeviews/ReactNodeView.tsx
@@ -289,12 +289,20 @@ export class SelectionBasedNodeView extends ReactNodeView {
     this.posEnd = this.pos + this.node.nodeSize;
   }
 
-  isSelectionInsideNode = (from: number, to: number) => {
-    if (typeof this.pos !== 'number' || typeof this.posEnd !== 'number') {
+  isSelectionInsideNode = (
+    from: number,
+    to: number,
+    pos?: number,
+    posEnd?: number,
+  ) => {
+    pos = typeof pos !== 'number' ? this.pos : pos;
+    posEnd = typeof posEnd !== 'number' ? this.posEnd : posEnd;
+
+    if (typeof pos !== 'number' || typeof posEnd !== 'number') {
       return false;
     }
 
-    return this.pos < from && to < this.posEnd;
+    return pos < from && to < posEnd;
   };
 
   insideSelection = () => {
@@ -312,13 +320,8 @@ export class SelectionBasedNodeView extends ReactNodeView {
     this.oldSelection = selection;
 
     // update cached positions
-    const { pos: oldPos } = this;
+    const { pos: oldPos, posEnd: oldPosEnd } = this;
     this.updatePos();
-
-    if (oldPos !== this.pos) {
-      // node has moved position
-      return true;
-    }
 
     const { from, to } = selection;
     const { from: oldFrom, to: oldTo } = oldSelection;
@@ -337,12 +340,19 @@ export class SelectionBasedNodeView extends ReactNodeView {
       }
     }
 
-    if (
-      (this.isSelectionInsideNode(from, to) &&
-        !this.isSelectionInsideNode(oldFrom, oldTo)) ||
-      (!this.isSelectionInsideNode(from, to) &&
-        this.isSelectionInsideNode(oldFrom, oldTo))
-    ) {
+    const movedInToSelection =
+      this.isSelectionInsideNode(from, to) &&
+      !this.isSelectionInsideNode(oldFrom, oldTo);
+
+    const movedOutOfSelection =
+      !this.isSelectionInsideNode(from, to) &&
+      this.isSelectionInsideNode(oldFrom, oldTo);
+
+    const moveOutFromOldSelection =
+      this.isSelectionInsideNode(from, to, oldPos, oldPosEnd) &&
+      !this.isSelectionInsideNode(from, to);
+
+    if (movedInToSelection || movedOutOfSelection || moveOutFromOldSelection) {
       return true;
     }
 