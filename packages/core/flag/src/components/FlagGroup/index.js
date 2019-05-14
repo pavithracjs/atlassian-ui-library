fa6d81c266e0b36f04d5782f71601b6dead54cd8
Lukasz Jarzembowski <ljarzembowski@atlassian.com>
2019-05-14 11:31:37 +0200

    AK-4470: Fixed incorrect flag appearing after dismiss

--- packages/core/flag/src/components/FlagGroup/index.js
+++ packages/core/flag/src/components/FlagGroup/index.js
@@ -4,7 +4,7 @@ import { Transition } from 'react-transition-group';
 import Portal from '@atlaskit/portal';
 import { layers } from '@atlaskit/theme';
 
-import Wrapper from '../../styled/Wrapper';
+import Wrapper, { flagAnimationTime } from '../../styled/Wrapper';
 import Group, { SROnly, Inner } from './styledFlagGroup';
 import type { ChildrenType, FunctionType } from '../../types';
 
@@ -31,7 +31,15 @@ export default class FlagGroup extends Component<Props, {}> {
         <Transition
           key={id}
           addEndListener={(node, done) => {
-            node.addEventListener('animationend', done);
+            node.addEventListener('animationstart', (...args) => {
+              setTimeout(() => done(...args), flagAnimationTime);
+            });
+            node.addEventListener('animationend', (...args) => {
+              done(...args);
+            });
+            if (idx > 0) {
+              done();
+            }
           }}
         >
           {transitionState => (
