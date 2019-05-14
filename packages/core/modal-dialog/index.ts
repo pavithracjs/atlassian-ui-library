55bee3d892b8e2f948b0c2430ce4cc3b90bfa8e0
Andrew Campbell <acampbell3@atlassian.com>
2019-05-14 17:18:21 +1000

    Remove .d.ts files and add index.ts

---
+++ packages/core/modal-dialog/index.ts
@@ -0,0 +1,5 @@
+import './typings/react-lorem-component';
+import './typings/react-scrolllock';
+
+export * from './src';
+export { default } from './src';
--- packages/core/modal-dialog/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Component, ReactNode, MouseEvent, KeyboardEvent } from 'react';
-import { Props } from './components/ModalWrapper';
-
-interface State {}
-
-export default class ModalDialog extends Component<Props, State> {}
