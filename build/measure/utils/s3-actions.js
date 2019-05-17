60372bb9ce138d30eec4afd77aea02615c19b580
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-05-17 11:48:14 +1000

    move logic to download

--- build/measure/utils/s3-actions.js
+++ build/measure/utils/s3-actions.js
@@ -19,14 +19,55 @@ function createDir(dir) {
   return dir;
 }
 
-function uploadToS3(pathToFile, branch) {
+function isAWSAccessible() {
   if (!AWS_ACCESS_KEY || !AWS_SECRET_KEY || !BITBUCKET_COMMIT) {
     console.error(
-      'AWS_ACCESS_KEY, AWS_SECRET_KEY or BITBUCKET_COMMIT are missing',
+      chalk.red(
+        'AWS_ACCESS_KEY, AWS_SECRET_KEY or BITBUCKET_COMMIT are missing',
+      ),
     );
     console.error(
       chalk.red('These env variables need to be set to be able to s3'),
     );
+    return false;
+  }
+  return true;
+}
+
+function downloadFromS3(pathToFolder, branch) {
+  if (!isAWSAccessible()) {
+    process.exit(1);
+  }
+
+  (async () => {
+    const workspaces = await bolt.getWorkspaces();
+    // We'll add the relativeDir's to these so we have more information to work from later
+    const packages = workspaces
+      .filter(ws => ws.dir.includes('/packages/'))
+      .map(
+        pkg => `${pkg.name.replace('@atlaskit/', '')}-bundle-size-ratchet.json`,
+      );
+
+    // TODO: Changes pkgs is not returning all changed pkgs
+    // console.log(process.env.CHANGED_PACKAGES);
+    // const changedPkg = process.env.CHANGED_PACKAGES
+    //   ? JSON.parse(process.env.CHANGED_PACKAGES).map(
+    //       pkg => `${pkg.split('/').reverse()[0]}-bundle-size-ratchet.json`,
+    //     )
+    //   : packages;
+
+    packages.forEach(ratchetFile => {
+      const bucketPath = `s3://${BUCKET_NAME}/${branch}/bundleSize/${ratchetFile}`;
+
+      npmRun.sync(
+        `s3-cli --region="${BUCKET_REGION}" get ${bucketPath} ${pathToFolder}/${ratchetFile}`,
+      );
+    });
+  })();
+}
+
+function uploadToS3(pathToFile, branch) {
+  if (!isAWSAccessible()) {
     process.exit(1);
   }
 
@@ -48,4 +89,9 @@ function uploadToS3(pathToFile, branch) {
   console.log(chalk.green('Successfully published to', publicUrl));
 }
 
-module.exports = { masterStatsFolder, currentStatsFolder, uploadToS3 };
+module.exports = {
+  masterStatsFolder,
+  currentStatsFolder,
+  uploadToS3,
+  downloadFromS3,
+};
