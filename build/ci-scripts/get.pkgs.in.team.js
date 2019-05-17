fb72dd92216b8ee51554184324c74c8779d63730
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-05-17 18:41:09 +1000

    add handling for editor and add other improvements

--- build/ci-scripts/get.pkgs.in.team.js
+++ build/ci-scripts/get.pkgs.in.team.js
@@ -3,11 +3,33 @@ const bolt = require('bolt');
 (async () => {
   const project = await bolt.getProject();
   const workspaces = await bolt.getWorkspaces();
-  const folder = process.argv[2];
+  let input = process.argv[2];
 
-  const packages = workspaces
-    .filter(ws => ws.dir.includes(`packages/${folder}/`))
-    .map(pkg => pkg.dir.replace(project.dir, ''));
+  // special handling for editor pkgs
+  if (input.includes('editor')) {
+    input = 'editor';
+  }
 
-  console.log(packages.join(' '));
+  const allFolders = input.split(',');
+
+  const packages = allFolders.map(folder => {
+    return workspaces
+      .filter(ws => ws.dir.includes(`packages/${folder}/`))
+      .map(pkg => pkg.dir.replace(project.dir, ''));
+  });
+  // flatten the map above
+  let pkgs = packages.reduce((acc, val) => acc.concat(val), []);
+
+  // apply split logic for editor pkgs
+  if (input.includes('editor')) {
+    const half = Math.floor(pkgs.length / 2);
+    if (process.argv[2].includes('part-1')) {
+      editorPkgs = pkgs.slice(0, half);
+    } else {
+      editorPkgs = pkgs.slice(half, pkgs.length);
+    }
+    pkgs = editorPkgs;
+  }
+
+  console.log(pkgs.join(' '));
 })();
