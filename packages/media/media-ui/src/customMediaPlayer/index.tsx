a5dabaa98c12dc67e56735cec19d424defb4aac6
Jose Nunez <jnunez@atlassian.com>
2019-05-20 16:38:36 +1000

    Add "is last played" shortcut handling approach

--- packages/media/media-ui/src/customMediaPlayer/index.tsx
+++ packages/media/media-ui/src/customMediaPlayer/index.tsx
@@ -110,7 +110,7 @@ export class CustomMediaPlayer extends Component<
 
   onFullScreenChange = () => {
     const { isFullScreenEnabled: currentFullScreenMode } = this.state;
-    const isFullScreenEnabled = getFullscreenElement() ? true : false;
+    const isFullScreenEnabled = getFullscreenElement() === this.videoWrapperRef;
 
     if (currentFullScreenMode !== isFullScreenEnabled) {
       this.setState({
@@ -130,7 +130,13 @@ export class CustomMediaPlayer extends Component<
   shortcutHandler = (toggleButtonAction: ToggleButtonAction) => () => {
     const { showControls } = this.props;
 
-    toggleButtonAction();
+    // Will only play by shortcut if it was the last played.
+    if (
+      toggleButtonAction !== this.play ||
+      this.simultaneousPlay.isLastPlayed()
+    ) {
+      toggleButtonAction();
+    }
 
     if (showControls) {
       showControls();
--- packages/media/media-ui/src/customMediaPlayer/simultaneousPlayManager.ts
+++ packages/media/media-ui/src/customMediaPlayer/simultaneousPlayManager.ts
@@ -3,18 +3,26 @@ type voidfn = () => void;
 export type SimultaneousPlaySubscription = {
   onPlay: voidfn;
   unsubscribe: voidfn;
+  isLastPlayed: () => boolean;
 };
 
 export default class SimultaneousPlayManager {
   private static playersPause = new Map<voidfn, voidfn>();
+
+  private static lastPlayed: voidfn;
 
   static subscribe = (pause: voidfn): SimultaneousPlaySubscription => {
     if (!SimultaneousPlayManager.playersPause.get(pause)) {
       SimultaneousPlayManager.playersPause.set(pause, pause);
     }
     return {
-      onPlay: () => SimultaneousPlayManager.onPlay(pause),
-      unsubscribe: () => SimultaneousPlayManager.unsubscribe(pause),
+      onPlay: () => {
+        SimultaneousPlayManager.onPlay(pause);
+      },
+      unsubscribe: () => {
+        SimultaneousPlayManager.unsubscribe(pause);
+      },
+      isLastPlayed: () => SimultaneousPlayManager.lastPlayed === pause,
     };
   };
 
@@ -23,6 +31,8 @@ export default class SimultaneousPlayManager {
   };
 
   private static onPlay = (pause: voidfn): void => {
+    SimultaneousPlayManager.lastPlayed = pause;
+
     SimultaneousPlayManager.playersPause.forEach(playerPause => {
       if (playerPause !== pause) playerPause();
     });
