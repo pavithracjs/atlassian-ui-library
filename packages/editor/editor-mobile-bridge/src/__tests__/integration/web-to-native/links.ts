8f885e1be5e56ac88cbc2237fb15f51ffec279c5
Fred Ghilini <fghilini@atlassian.com>
2019-05-20 13:38:12 +1000

    ED-6877: fixed integration tests testing links on editor bridge

--- packages/editor/editor-mobile-bridge/src/__tests__/integration/web-to-native/links.ts
+++ packages/editor/editor-mobile-bridge/src/__tests__/integration/web-to-native/links.ts
@@ -1,5 +1,7 @@
 import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
 import Page from '@atlaskit/webdriver-runner/wd-wrapper';
+import constant from 'lodash.constant';
+import times from 'lodash.times';
 
 import {
   callNativeBridge,
@@ -9,7 +11,6 @@ import {
   getBridgeOutput,
 } from '../_utils';
 
-// https://product-fabric.atlassian.net/browse/ED-6877
 BrowserTestCase(
   'currentSelection when no selection',
   // Safari has issues with key events
@@ -41,11 +42,12 @@ BrowserTestCase(
     await browser.waitForSelector(editable);
 
     await browser.type(editable, 'Normal Text');
-
-    await browser.pressKey('ArrowLeft', 4);
-    await browser.keys('Shift');
-    await browser.pressKey('ArrowRight', 4);
-    await browser.keys('Shift');
+    await browser.keys([
+      ...times(4, constant('ArrowLeft')),
+      'Shift',
+      ...times(4, constant('ArrowRight')),
+      'Shift',
+    ]);
 
     const currentSelection = await getBridgeOutput(
       browser,
@@ -83,7 +85,6 @@ BrowserTestCase(
   },
 );
 
-// https://product-fabric.atlassian.net/browse/ED-6877
 BrowserTestCase(
   'currentSelection when cursor is selecting a link',
   // Safari has issues with key events
@@ -101,10 +102,12 @@ BrowserTestCase(
       'https://www.google.com',
     );
 
-    await browser.pressKey('ArrowLeft', 9);
-    await browser.keys('Shift');
-    await browser.pressKey('ArrowRight', 9);
-    await browser.keys('Shift');
+    await browser.keys([
+      ...times(9, constant('ArrowLeft')),
+      'Shift',
+      ...times(9, constant('ArrowRight')),
+      'Shift',
+    ]);
 
     const currentSelection = await getBridgeOutput(
       browser,
@@ -132,10 +135,12 @@ BrowserTestCase(
       'https://www.google.com',
     );
 
-    await browser.pressKey('ArrowLeft', 10);
-    await browser.keys('Shift');
-    await browser.pressKey('ArrowRight', 10);
-    await browser.keys('Shift');
+    await browser.keys([
+      ...times(10, constant('ArrowLeft')),
+      'Shift',
+      ...times(10, constant('ArrowRight')),
+      'Shift',
+    ]);
 
     const currentSelection = await getBridgeOutput(
       browser,
