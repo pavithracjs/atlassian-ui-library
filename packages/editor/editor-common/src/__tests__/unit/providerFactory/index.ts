fd5883356c1b60d579dbe211c3c320b003c974a2
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-05-21 13:42:38 +1000

    revert changes made for testing

--- packages/editor/editor-common/src/__tests__/unit/providerFactory/index.ts
+++ packages/editor/editor-common/src/__tests__/unit/providerFactory/index.ts
@@ -1,6 +1,5 @@
 import ProviderFactory from '../../../providerFactory';
 
-// adding command for testing
 describe('ProviderFactory', () => {
   const provider = Promise.resolve('Hello');
   const providerName1 = 'greetingProvider';
