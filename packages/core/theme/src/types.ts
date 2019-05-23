36a89a391aeb7d52e46d5e5b391383456e374769
Thiago de Bastos <thiago@thiagodebastos.com>
2019-05-23 15:33:33 +1000

    add elevation types

--- packages/core/theme/src/types.ts
+++ packages/core/theme/src/types.ts
@@ -10,3 +10,5 @@ export type ThemedValue = (props?: ThemeProps) => string | number;
 
 // Non-deprecated types
 export type colorPaletteType = '8' | '16' | '24';
+
+export type Elevation = 'e100' | 'e200' | 'e300' | 'e400' | 'e500';
