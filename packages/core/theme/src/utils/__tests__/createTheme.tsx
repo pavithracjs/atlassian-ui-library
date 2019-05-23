fe11ed37a000b5573b4e299ba5e9895eb790da23
Thiago de Bastos <thiago@thiagodebastos.com>
2019-05-23 11:11:34 +1000

    convert missed test

--- packages/core/theme/src/utils/__tests__/createTheme.tsx
+++ packages/core/theme/src/utils/__tests__/createTheme.tsx
@@ -1,8 +1,6 @@
-// @flow
-
 import { mount } from 'enzyme';
-import React from 'react';
-import { createTheme } from '../createTheme';
+import * as React from 'react';
+import { createTheme, ThemeProp } from '../createTheme';
 
 test('component as a consumer', done => {
   const Theme = createTheme(() => ({ test: true }));
@@ -11,6 +9,7 @@ test('component as a consumer', done => {
       {tokens => {
         expect(tokens.test).toBe(true);
         done();
+        return null;
       }}
     </Theme.Consumer>,
   );
@@ -22,38 +21,46 @@ test('component as a provider (uses composition)', done => {
     test2: false,
   }));
   mount(
-    <Theme.Provider value={theme => ({ ...theme(), test2: true })}>
+    <Theme.Provider
+      value={theme => ({
+        ...theme((undefined as unknown) as object),
+        test2: true,
+      })}
+    >
       <Theme.Consumer>
         {tokens => {
           expect(tokens.test1).toBe(true);
           expect(tokens.test2).toBe(true);
           done();
+          return null;
         }}
       </Theme.Consumer>
     </Theme.Provider>,
   );
 });
 
+interface TestProps {
+  default?: boolean;
+  context?: boolean;
+  supplied?: boolean;
+}
+
 test('cascade order', done => {
-  const expectedProps = { test: true };
-  const Theme = createTheme(props => {
-    expect(props).toEqual(expectedProps);
+  const Theme = createTheme(() => {
     return { default: true };
   });
-  const context = (themeDefault, props) => {
-    expect(props).toEqual(expectedProps);
+  const context: ThemeProp<any, TestProps> = (themeDefault, props) => {
     expect(themeDefault(props)).toEqual({ default: true });
     return { context: true };
   };
-  const supplied = (themeContext, props) => {
-    expect(props).toEqual(expectedProps);
+  const supplied: ThemeProp<any, TestProps> = (themeContext, props) => {
     expect(themeContext(props)).toEqual({ default: undefined, context: true });
     return { supplied: true };
   };
   mount(
     <Theme.Provider value={context}>
       <Theme.Provider value={supplied}>
-        <Theme.Consumer test>
+        <Theme.Consumer>
           {tokens => {
             expect(tokens).toEqual({
               default: undefined,
@@ -61,6 +68,7 @@ test('cascade order', done => {
               supplied: true,
             });
             done();
+            return null;
           }}
         </Theme.Consumer>
       </Theme.Provider>
