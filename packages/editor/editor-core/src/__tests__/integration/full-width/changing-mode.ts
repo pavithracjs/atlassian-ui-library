280b47f8f4a99f4a3d94d25e6d71bcc74442a468
Tim Keir <tkeir@atlassian.com>
2019-05-24 10:29:13 +1000

    CHANGESET: gold-sheep-buy. ED-6701 Upgrade prosemirror-view to 1.9.9 and prosemirror-inputrules to 1.0.4 for composition input improvements
    Upgraded to leverage composition input improvements.
    
    - Refactored prosemirror-view private API around composition
    - Removed domObserver.flush() from typeahead plugin (as auto done by PM 1.9.x)
    - Minor adjustments for impacted unit, integration, and VR tests
    - Update ratchets to reflect updated deps
    - Regen VR snapshots for dynamic text sizing and gutters.
    
    CHANGESET: gold-sheep-buy. ED-6701 Upgrade prosemirror-view to 1.9.9 and prosemirror-inputrules to 1.0.4 for composition input improvements

--- packages/editor/editor-core/src/__tests__/integration/full-width/changing-mode.ts
+++ packages/editor/editor-core/src/__tests__/integration/full-width/changing-mode.ts
@@ -13,7 +13,8 @@ import { mediumSizeDoc } from './__fixtures__/medium-document';
 
 BrowserTestCase(
   'Should transition successfully, without error, when a selection over react nodes exists',
-  { skip: ['ie'] },
+  // `page.checkConsoleErrors` only runs on Chrome so we skip all other browsers.
+  { skip: ['ie', 'edge', 'safari', 'firefox'] },
   async (client: any) => {
     const page = await goToEditorTestingExample(client);
     await mountEditor(page, {
@@ -33,6 +34,8 @@ BrowserTestCase(
 
     await setProseMirrorTextSelection(page, { anchor: 314, head: 308 });
     await updateEditorProps(page, { appearance: 'full-width' });
+
+    // Chrome only check
     await page.checkConsoleErrors();
   },
 );
