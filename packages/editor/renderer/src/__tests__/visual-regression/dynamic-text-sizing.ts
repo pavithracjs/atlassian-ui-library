16dccc7bd40b24c016f725742072aa3c696b26af
Heather Roberts <hroberts@atlassian.com>
2019-05-27 14:40:35 +1000

    ED-6954 Reduce threshold in renderer VR tests to 0.001
    Refactor renderer VR tests to be more like editor-core
    Fix some issues with flaky tests

--- packages/editor/renderer/src/__tests__/visual-regression/dynamic-text-sizing.ts
+++ packages/editor/renderer/src/__tests__/visual-regression/dynamic-text-sizing.ts
@@ -1,29 +1,39 @@
-import { snapshot, mountRenderer, goToRendererTestingExample } from './_utils';
-import { document } from './__fixtures__/document-without-media';
+import { MINIMUM_THRESHOLD } from '@atlaskit/visual-regression/helper';
+import { snapshot, initRendererWithADF } from './_utils';
+import * as document from '../__fixtures__/document-without-media.adf.json';
 import { Page } from 'puppeteer';
+
+const initRenderer = async (
+  page: Page,
+  viewport: { width: number; height: number },
+) => {
+  await initRendererWithADF(page, {
+    appearance: 'full-page',
+    viewport,
+    rendererProps: { allowDynamicTextSizing: true },
+    adf: document,
+  });
+};
 
 describe('Snapshot Test: Dynamic Text Sizing', () => {
   let page: Page;
-  beforeAll(async () => {
+  beforeAll(() => {
     // @ts-ignore
     page = global.page;
-    await goToRendererTestingExample(page);
+  });
+
+  afterEach(async () => {
+    await page.waitFor(100); // wait for dynamic text sizing to occur
+    await snapshot(page, MINIMUM_THRESHOLD);
   });
 
   [
     { width: 1440, height: 3200 },
     { width: 1120, height: 3000 },
     { width: 1000, height: 3000 },
-  ].forEach(size => {
-    it(`should correctly render ${size.width}`, async () => {
-      await page.setViewport(size);
-      await page.waitFor(100);
-      await mountRenderer(page, {
-        appearance: 'full-page',
-        allowDynamicTextSizing: true,
-        document,
-      });
-      await snapshot(page, 0.01);
+  ].forEach(viewport => {
+    it(`should correctly render ${viewport.width}`, async () => {
+      await initRenderer(page, viewport);
     });
   });
 });
