4d72d63e375bcc9e26fb3f086543b856eafc54b8
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2019-05-29 10:03:40 +1000

    Add more tests for withMediaClient

--- packages/media/media-client/src/utils/__tests__/unit/with-media-client-hoc.spec.tsx
+++ packages/media/media-client/src/utils/__tests__/unit/with-media-client-hoc.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { shallow, mount } from 'enzyme';
-import { Context } from '@atlaskit/media-core';
+import { mount } from 'enzyme';
+import { Context, MediaClientConfig } from '@atlaskit/media-core';
 import { withMediaClient, WithMediaClient } from '../../with-media-client-hoc';
 
 const DummyComponent = (props: WithMediaClient) => null;
@@ -13,5 +13,43 @@ describe('withMediaClient', () => {
     expect(
       component.find<WithMediaClient>(DummyComponent).props().mediaClient,
     ).toEqual(context);
+  });
+
+  it('should create new mediaClient from given mediaClientConfig', () => {
+    const Wrapper = withMediaClient(DummyComponent);
+    const mediaClientConfig: MediaClientConfig = {
+      authProvider: () =>
+        Promise.resolve({
+          baseUrl: 'url',
+          clientId: 'client-id',
+          token: 'token',
+        }),
+    };
+    const component = mount(<Wrapper mediaClientConfig={mediaClientConfig} />);
+    const mediaClient = component.find<WithMediaClient>(DummyComponent).props()
+      .mediaClient;
+    expect(mediaClient).not.toBeUndefined();
+    expect(mediaClient.config).toEqual(mediaClientConfig);
+  });
+
+  it('should reuse previously created mediaClient instance for same mediaClientConfig', () => {
+    const Wrapper = withMediaClient(DummyComponent);
+    const mediaClientConfig: MediaClientConfig = {
+      authProvider: () =>
+        Promise.resolve({
+          baseUrl: 'url',
+          clientId: 'client-id',
+          token: 'token',
+        }),
+    };
+    const component1 = mount(<Wrapper mediaClientConfig={mediaClientConfig} />);
+    const component2 = mount(<Wrapper mediaClientConfig={mediaClientConfig} />);
+    const mediaClient1 = component1
+      .find<WithMediaClient>(DummyComponent)
+      .props().mediaClient;
+    const mediaClient2 = component2
+      .find<WithMediaClient>(DummyComponent)
+      .props().mediaClient;
+    expect(mediaClient1).toBe(mediaClient2);
   });
 });
