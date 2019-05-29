1897dbef596b2fa050958321533d51c476f1cb06
Thiago de Bastos <thiago@thiagodebastos.com>
2019-05-29 14:54:28 +1000

    export GlobalThemeTokens so that it can be used in Badge

--- packages/core/theme/src/types.ts
+++ packages/core/theme/src/types.ts
@@ -1,11 +1,16 @@
 // Deprecated / legacy types
 export type ThemeModes = 'light' | 'dark';
+
 export interface Theme {
   mode: ThemeModes;
 }
+
+export interface GlobalThemeTokens extends Theme {}
+
 export interface ThemeProps {
   theme: { __ATLASKIT_THEME__: Theme };
 }
+
 export type ThemedValue = (props?: ThemeProps) => string | number;
 
 // Non-deprecated types
