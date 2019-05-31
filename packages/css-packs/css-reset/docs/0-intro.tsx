6bf2e6547a5124e2ac67febd4b6858b0dafebff6
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2019-05-31 10:17:24 +1000

    Add index.ts to .npmignore

--- packages/css-packs/css-reset/docs/0-intro.tsx
+++ packages/css-packs/css-reset/docs/0-intro.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { md, code, Example } from '@atlaskit/docs';
 
 export default md`
--- packages/css-packs/css-reset/examples/08-code-and-pre.tsx
+++ packages/css-packs/css-reset/examples/08-code-and-pre.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 /**
  * By default the Atlaskit website includes css-reset in examples
