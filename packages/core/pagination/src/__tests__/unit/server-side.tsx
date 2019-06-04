c91e91113b6d9b6637aa3b6eff7314e3593aca4d
raja <rbellebon@atlassian.com>
2019-06-04 16:55:42 +1000

    fix pagination tests

--- packages/core/pagination/src/__tests__/unit/server-side.tsx
+++ packages/core/pagination/src/__tests__/unit/server-side.tsx
@@ -8,11 +8,9 @@ import ReactDOMServer from 'react-dom/server';
 type Example = { filePath: string };
 
 test('Pavigation server side rendering', async done => {
-  (await getExamplesFor('pagination')).forEach((example: Example) => {
-    // $StringLitteral
+  (await getExamplesFor('pagination')).forEach(async (example: Example) => {
     if (!example.filePath.includes('react-router')) {
       /* react router example contains import on react-router-dom */
-      // $StringLitteral
       const Example = await require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
       expect(() =>
         ReactDOMServer.renderToString(<Example />),
