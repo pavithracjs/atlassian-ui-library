8049012afeb5a3e48119893a8a894e407a509334
Sean Curtis <scurtis@atlassian.com>
2019-06-06 12:55:36 +1000

    The Portal rendered for a Drawer does not mount until the portal is made visible

---
+++ packages/core/drawer/src/components/__tests__/unit/portals.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Drawer from '../../index';
+
+describe('Drawer portal', () => {
+  it('should not be rendered if not open', () => {
+    mount(<Drawer />);
+
+    const renderedPortal = document.querySelector(
+      'body > .atlaskit-portal-container .atlaskit-portal',
+    );
+    expect(renderedPortal).toBe(null);
+  });
+});
