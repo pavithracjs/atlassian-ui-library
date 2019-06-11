c498316726ce9c951b3c833ec8a8e1f93ed9ca18
Martin Picha <mpicha@atlassian.com>
2019-06-11 16:50:48 +1000

    AK-6305 - Typo when clicking in file

--- packages/core/datetime-picker/src/internal/__tests__/unit/DatePicker.js
+++ packages/core/datetime-picker/src/internal/__tests__/unit/DatePicker.js
@@ -31,7 +31,7 @@ test('DatePicker, onCalendarChange if the iso date is greater than the last day 
   const datePickerWrapper = mount(<DatePicker />);
   datePickerWrapper.instance().onCalendarChange({ iso: date });
   datePickerWrapper.update();
-  expect(datePickerWrapper.instance().state.view).toEqual(fallbakcDate);
+  expect(datePickerWrapper.instance().state.view).toEqual(fallbackDate);
 });
 
 test('DatePicker, supplying a custom parseInputValue prop, produces the expected result', () => {
