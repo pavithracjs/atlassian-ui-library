7aaf666c748c191c9268b7ee117066ffa9e768c3
Mauro Alvarez <contact@mauroalvarez.com>
2019-06-12 17:37:18 -0300

    help-panel - fix CSS issues

--- packages/help/help-panel/src/components/HelpPanelDrawer/constants.tsx
+++ packages/help/help-panel/src/components/HelpPanelDrawer/constants.tsx
@@ -1,3 +1,4 @@
 export const transitionDurationMs = 220;
 export const transitionTimingFunction = 'cubic-bezier(0.2, 0, 0, 1)';
 export const panelWidth = 368;
+export const panelBorderWidth = 3;
--- packages/help/help-panel/src/components/HelpPanelDrawer/styled.ts
+++ packages/help/help-panel/src/components/HelpPanelDrawer/styled.ts
@@ -1,22 +1,22 @@
 /** @jsx jsx */
 import styled from '@emotion/styled';
 import * as colors from '@atlaskit/theme/colors';
-import { panelWidth } from './constants';
+import { panelWidth, panelBorderWidth } from './constants';
 
 export const HelpDrawer = styled.div`
   background-color: white;
   width: ${panelWidth}px;
   flex: 0 0 ${panelWidth}px;
   position relative;
+  overflow: hidden;
 `;
 
 export const HelpDrawerContent = styled.div`
   flex: 1;
-  border-left: 3px solid ${colors.N30};
-  overflow: auto;
+  border-left: ${panelBorderWidth}px solid ${colors.N30};
+  overflow: hidden;
   flex-direction: column;
-  width: ${panelWidth}px;
+  width: ${panelWidth - panelBorderWidth}px;
   height: 100%;
-  overflow-y: auto;
   position: fixed;
 `;
