0e5811b8c6489da80a97538f9119cdad70f04b11
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-06-21 17:27:38 +1000

    skip landkid test failing dueto buildtools-117

--- packages/navigation/atlassian-switcher/src/__tests__/unit/ssr-compatibility.tsx
+++ packages/navigation/atlassian-switcher/src/__tests__/unit/ssr-compatibility.tsx
@@ -3,7 +3,7 @@
  * other dependant packages from blowing up we need to test that AtlassianSwitcher
  * could be safely imported in `node` environment
  */
-describe('Atlassian Switcher - SSR', async () => {
+describe('Atlassian Switcher - SSR', () => {
   it('should not break when importing AtlassianSwitcher package', () => {
     expect(() => {
       // @ts-ignore
