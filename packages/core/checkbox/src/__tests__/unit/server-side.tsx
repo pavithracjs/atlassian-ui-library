167ce643c4407e98fc7bf504740f4a2fa64a33ae
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-07-03 17:09:10 +1000

    add wait for expect on missing tests

--- packages/core/checkbox/src/__tests__/unit/server-side.tsx
+++ packages/core/checkbox/src/__tests__/unit/server-side.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
 import ReactDOMServer from 'react-dom/server';
+import waitForExpect from 'wait-for-expect';
 
 test.skip('Checkbox server side rendering', async done => {
   const examples = await getExamplesFor('checkbox');
   for (const example of examples) {
     const Example = await require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
-    expect(() => ReactDOMServer.renderToString(<Example />)).not.toThrowError();
+    await waitForExpect(() => {
+      expect(() =>
+        ReactDOMServer.renderToString(<Example />),
+      ).not.toThrowError();
+    });
   }
-  done();
 });
--- packages/media/media-picker/src/__tests__/unit/media-picker-server-side.tsx
+++ packages/media/media-picker/src/__tests__/unit/media-picker-server-side.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
+import waitForExpect from 'wait-for-expect';
 
 test.skip('media-picker server side rendering', async done => {
   const examples = await getExamplesFor('media-picker');
   for (const example of examples) {
     const Example = await require(example.filePath).default;
-    expect(() => ReactDOMServer.renderToString(<Example />)).not.toThrowError();
+    waitForExpect(() => {
+      expect(() =>
+        ReactDOMServer.renderToString(<Example />),
+      ).not.toThrowError();
+    });
   }
-  done();
 });
