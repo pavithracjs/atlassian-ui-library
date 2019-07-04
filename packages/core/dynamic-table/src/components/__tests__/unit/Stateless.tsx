e88c4cbc41b193025bf134f3870c3caeae9cc0d3
Will Mendes <willmendesneto@gmail.com>
2019-07-04 21:55:07 +1000

    fix(dynamic-table): making Stateless test faster

--- packages/core/dynamic-table/src/components/__tests__/unit/Stateless.tsx
+++ packages/core/dynamic-table/src/components/__tests__/unit/Stateless.tsx
@@ -23,110 +23,110 @@ const createProps: () => StatelessProps = () => ({
   onPageRowsUpdate: jest.fn(),
 });
 
-test('onSort should change to ASC from DESC if table is not rankable', () => {
-  const props = createProps();
-  const wrapper = shallow(
-    <StatelessDynamicTable {...props} sortOrder="DESC" />,
-  );
-
-  const item = simulateOnSort(wrapper);
-  expect(props.onSort).toHaveBeenCalledTimes(1);
-  expect(props.onSort).toHaveBeenLastCalledWith({
-    key: sortKey,
-    item,
-    sortOrder: 'ASC',
-  });
-});
-
-test('onSort should change to none if table is rankable and sort order was DESC', () => {
-  const props = createProps();
-  const wrapper = shallow(
-    <StatelessDynamicTable {...props} sortOrder="DESC" isRankable />,
-  );
-
-  const item = simulateOnSort(wrapper);
-  expect(props.onSort).toHaveBeenCalledTimes(1);
-  expect(props.onSort).toHaveBeenLastCalledWith({
-    key: null,
-    item,
-    sortOrder: null,
-  });
-});
-
-test('onSort should change to DESC if table is rankable and sort order was ASC', () => {
-  const props = createProps();
-  const wrapper = shallow(
-    <StatelessDynamicTable {...props} sortOrder="ASC" isRankable />,
-  );
-
-  const item = simulateOnSort(wrapper);
-  expect(props.onSort).toHaveBeenCalledTimes(1);
-  expect(props.onSort).toHaveBeenLastCalledWith({
-    key: sortKey,
-    item,
-    sortOrder: 'DESC',
-  });
-});
-
-test('onSort should change to ASC if table is rankable and was sorted using on different row', () => {
-  const props = createProps();
-  const wrapper = shallow(
-    <StatelessDynamicTable
-      {...props}
-      sortOrder="DESC"
-      sortKey={secondSortKey}
-      isRankable
-    />,
-  );
-
-  const item = simulateOnSort(wrapper);
-  expect(props.onSort).toHaveBeenCalledTimes(1);
-  expect(props.onSort).toHaveBeenLastCalledWith({
-    key: sortKey,
-    item,
-    sortOrder: 'ASC',
-  });
-});
-
-test('onPageRowsUpdate should be called on mount and on sorting change', () => {
-  const props = createProps();
-  const wrapper = mount(<StatelessDynamicTable {...props} />);
-
-  expect(props.onPageRowsUpdate).toHaveBeenCalledTimes(1);
-  wrapper.setProps({ sortOrder: 'DESC' });
-  expect(props.onPageRowsUpdate).toHaveBeenCalledTimes(2);
-});
-// TODO: Raise it to the team.
-test.skip('Mount should throw errors if the sortKey is invalid', () => {
-  const props = { ...createProps(), sortKey: 'InvalidSortKey' };
-  mount(<StatelessDynamicTable {...props} />);
-
-  /* eslint-disable no-console */
-  expect(console.error).toHaveBeenCalled();
-  expect(console.error).toBeCalledWith(
-    Error(`Cell with InvalidSortKey key not found in head.`),
-  );
-  /* eslint-enable no-console */
-});
-
-describe('DynamicTableWithAnalytics', () => {
+describe('Stateless', () => {
   beforeEach(() => {
-    jest.spyOn(global.console, 'warn');
-    jest.spyOn(global.console, 'error');
+    jest.spyOn(console, 'warn');
+    jest.spyOn(console, 'error');
   });
+
   afterEach(() => {
-    // @ts-ignore - property 'mockRestore' does not exist
-    global.console.warn.mockRestore();
+    jest.resetAllMocks();
+  });
+  describe('DynamicTableWithoutAnalytics', () => {
+    it('onSort should change to ASC from DESC if table is not rankable', () => {
+      const props = createProps();
+      const wrapper = shallow(
+        <StatelessDynamicTable {...props} sortOrder="DESC" />,
+      );
 
-    // @ts-ignore - property 'mockRestore' does not exist
-    global.console.error.mockRestore();
+      const item = simulateOnSort(wrapper);
+      expect(props.onSort).toHaveBeenCalledTimes(1);
+      expect(props.onSort).toHaveBeenLastCalledWith({
+        key: sortKey,
+        item,
+        sortOrder: 'ASC',
+      });
+    });
+
+    it('onSort should change to none if table is rankable and sort order was DESC', () => {
+      const props = createProps();
+      const wrapper = shallow(
+        <StatelessDynamicTable {...props} sortOrder="DESC" isRankable />,
+      );
+
+      const item = simulateOnSort(wrapper);
+      expect(props.onSort).toHaveBeenCalledTimes(1);
+      expect(props.onSort).toHaveBeenLastCalledWith({
+        key: null,
+        item,
+        sortOrder: null,
+      });
+    });
+
+    it('onSort should change to DESC if table is rankable and sort order was ASC', () => {
+      const props = createProps();
+      const wrapper = shallow(
+        <StatelessDynamicTable {...props} sortOrder="ASC" isRankable />,
+      );
+
+      const item = simulateOnSort(wrapper);
+      expect(props.onSort).toHaveBeenCalledTimes(1);
+      expect(props.onSort).toHaveBeenLastCalledWith({
+        key: sortKey,
+        item,
+        sortOrder: 'DESC',
+      });
+    });
+
+    it('onSort should change to ASC if table is rankable and was sorted using on different row', () => {
+      const props = createProps();
+      const wrapper = shallow(
+        <StatelessDynamicTable
+          {...props}
+          sortOrder="DESC"
+          sortKey={secondSortKey}
+          isRankable
+        />,
+      );
+
+      const item = simulateOnSort(wrapper);
+      expect(props.onSort).toHaveBeenCalledTimes(1);
+      expect(props.onSort).toHaveBeenLastCalledWith({
+        key: sortKey,
+        item,
+        sortOrder: 'ASC',
+      });
+    });
+
+    it('onPageRowsUpdate should be called on mount and on sorting change', () => {
+      const props = createProps();
+      const wrapper = mount(<StatelessDynamicTable {...props} />);
+
+      expect(props.onPageRowsUpdate).toHaveBeenCalledTimes(1);
+      wrapper.setProps({ sortOrder: 'DESC' });
+      expect(props.onPageRowsUpdate).toHaveBeenCalledTimes(2);
+    });
+
+    it('Mount should throw errors if the sortKey is invalid', () => {
+      const props = { ...createProps(), sortKey: 'InvalidSortKey' };
+      mount(<StatelessDynamicTable {...props} />);
+
+      /* eslint-disable no-console */
+      expect(console.error).toHaveBeenCalled();
+      expect(console.error).toBeCalledWith(
+        Error(`Cell with InvalidSortKey key not found in head.`),
+      );
+      /* eslint-enable no-console */
+    });
   });
 
-  it('should mount without errors', () => {
-    mount(<DynamicTableWithAnalytics {...createProps()} />);
-    /* eslint-disable no-console */
-    expect(console.warn).not.toHaveBeenCalled();
-    expect(console.error).not.toHaveBeenCalled();
-    /* eslint-enable no-console */
+  describe('DynamicTableWithAnalytics', () => {
+    it('should mount without errors', () => {
+      mount(<DynamicTableWithAnalytics {...createProps()} />);
+      /* eslint-disable no-console */
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+      /* eslint-enable no-console */
+    });
   });
 });
