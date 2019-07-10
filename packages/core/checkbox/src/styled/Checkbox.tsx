8b0dbf5a9d0b093b9fd256eb9dba8147238f8f4d
gwyneplaine <cc.lee@live.com.au>
2019-07-10 16:02:25 +1000

    init implement theme in checkbox

---
+++ packages/core/checkbox/src/styled/Checkbox.tsx
@@ -0,0 +1,219 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import { themed } from '@atlaskit/theme/components';
+import { gridSize } from '@atlaskit/theme/constants';
+import * as colors from '@atlaskit/theme/colors';
+import { multiply } from '@atlaskit/theme/math';
+import React from 'react';
+
+export const HiddenCheckbox = React.forwardRef((
+  // @ts-ignore - createAnalyticsEvent is injected from WithAnalyticsEvents HOC
+  { createAnalyticsEvent, ...props }: React.HTMLProps<HTMLInputElement>,
+  ref: React.Ref<HTMLInputElement>,
+) => (
+  <input
+    ref={ref}
+    css={css`
+      left: 50%;
+      margin: 0;
+      opacity: 0;
+      padding: 0;
+      position: absolute;
+      transform: translate(-50%, -50%);
+      top: 50%;
+    `}
+    {...props}
+  />
+));
+
+const disabledColor = themed({ light: colors.N80, dark: colors.N80 });
+
+interface Props {
+  isActive?: boolean;
+  isChecked?: boolean | unknown;
+  isDisabled?: boolean;
+  isFocused?: boolean;
+  isInvalid?: boolean;
+  isHovered?: boolean;
+  rest?: any;
+}
+
+interface LabelProps extends React.HTMLProps<HTMLLabelElement> {
+  isDisabled?: boolean;
+}
+export const Label = ({ isDisabled, ...rest }: LabelProps) => (
+  <label
+    css={css`
+      align-items: flex-start;
+      display: flex;
+      color: ${isDisabled ? disabledColor(rest) : colors.text(rest)};
+      ${isDisabled
+        ? css`
+            cursor: not-allowed;
+          `
+        : ''};
+    `}
+    {...rest}
+  />
+);
+
+const activeBorder = iconTokens => ({
+  stroke: iconTokens.borderColor.active,
+  strokeWidth: '2px',
+});
+
+const checkedBorder = iconTokens => ({
+  stroke: iconTokens.borderColor.checked,
+  strokeWidth: '2px',
+});
+
+const focusBorder = iconTokens => ({
+  stroke: iconTokens.borderColor.focused,
+  strokeWidth: '2px;',
+});
+
+const invalidBorder = iconTokens => ({
+  stroke: iconTokens.borderColor.invalid,
+  strokeWidth: '2px;',
+});
+
+const border = ({ isHovered, tokens: { icon }, ...rest }: Props) => css`
+  stroke: ${isHovered ? icon.borderColor.hovered : icon.borderColor.rest};
+  stroke-width: 2px;
+`;
+
+const getBorderColor = ({ tokens, ...props }: Props) => {
+  if (props.isDisabled) {
+    return tokens.icon.borderColor.disabled;
+  }
+  if (props.isActive) {
+    return activeBorder(tokens.icon);
+  }
+  if (props.isChecked) {
+    return checkedBorder(tokens.icon);
+  }
+  if (props.isFocused) {
+    return focusBorder(tokens.icon);
+  }
+  if (props.isInvalid) {
+    return invalidBorder(tokens.icon);
+  }
+  return border({ tokens, ...props });
+};
+
+const getTickColor = (props: Props) => {
+  const {
+    isChecked,
+    isDisabled,
+    isActive,
+    tokens: { icon },
+    ...rest
+  } = props;
+
+  let color = icon.tickColor.checked;
+
+  if (isDisabled && isChecked) {
+    color = icon.tickColor.disabledAndChecked;
+  } else if (isActive && isChecked && !isDisabled) {
+    color = icon.tickColor.isActiveAndChecked;
+  } else if (!isChecked) {
+    color = icon.tickColor.rest;
+  }
+  return color;
+};
+
+const getBoxColor = (props: Props) => {
+  const {
+    isChecked,
+    isDisabled,
+    isActive,
+    isHovered,
+    tokens: { icon },
+    ...rest
+  } = props;
+  // set the default
+  let color = icon.boxColor.rest;
+
+  if (isDisabled) {
+    color = icon.boxColor.disabled;
+  } else if (isActive) {
+    color = icon.boxColor.active;
+  } else if (isHovered && isChecked) {
+    color = icon.boxColor.hoveredAndChecked;
+  } else if (isHovered) {
+    color = icon.boxColor.hovered;
+  } else if (isChecked) {
+    color = icon.boxColor.checked;
+  }
+  return color;
+};
+
+export const LabelText = (props: { children: React.ReactNode }) => (
+  <span
+    css={css`
+      padding: 6px 4px;
+    `}
+    {...props}
+  />
+);
+
+export const CheckboxWrapper = (props: { children: React.ReactNode }) => (
+  <span
+    css={{
+      display: 'flex;',
+      flexShrink: 0,
+      position: 'relative',
+    }}
+    {...props}
+  />
+);
+
+interface IconProps extends React.HTMLProps<HTMLLabelElement> {
+  isChecked?: boolean;
+  isDisabled?: boolean;
+  isActive?: boolean;
+  isHovered?: boolean;
+  isFocused?: boolean;
+  isInvalid?: boolean;
+}
+export const IconWrapper = ({ children, ...props }: IconProps) => (
+  <span
+    css={{
+      lineHeight: 0,
+      flexShrink: 0,
+      color: getBoxColor(props),
+      fill: getTickColor(props),
+      transition: 'all 0.2s ease-in-out;',
+
+      /* This is adding a property to the inner svg, to add a border to the checkbox */
+      '& rect:first-of-type': {
+        transition: 'stroke 0.2s ease-in-out;',
+        ...getBorderColor(props),
+      },
+
+      /**
+       * Need to set the Icon component wrapper to flex to avoid a scrollbar bug which
+       * happens when checkboxes are flex items in a parent with overflow.
+       * See AK-6321 for more details.
+       **/
+      '> span': {
+        display: 'flex',
+      },
+    }}
+    children={children}
+  />
+);
+
+export type RequiredIndicatorProps = {
+  'aria-hidden'?: boolean | 'true' | 'false';
+} & React.AllHTMLAttributes<HTMLSpanElement>;
+
+export const RequiredIndicator = (props: RequiredIndicatorProps) => (
+  <span
+    css={{
+      color: colors.R400,
+      paddingLeft: `${math.multiply(gridSize, 0.25)}px;`,
+    }}
+    {...props}
+  />
+);
