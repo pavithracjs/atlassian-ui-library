5737c5c9370f4f871fc53281949a96d3acff1d9a
Luiz Pereira <lpereira@atlassian.com>
2019-07-11 08:55:25 +1000

    FS-4008 add translations

--- packages/elements/share/src/i18n/it.ts
+++ packages/elements/share/src/i18n/it.ts
@@ -10,21 +10,36 @@ export default {
   'fabric.elements.share.form.send': 'Invia',
   'fabric.elements.share.form.retry': 'Riprova',
   'fabric.elements.share.form.comment.placeholder': 'Aggiungi un messaggio',
-  'fabric.elements.share.form.user-picker.placeholder':
+  'fabric.elements.share.form.user-picker.placeholder.generic':
     'Inserisci nome, team o e-mail',
+  'fabric.elements.share.form.user-picker.placeholder.existingUserOnly':
+    'Inserisci nome o team',
   'fabric.elements.share.form.user-picker.add-more': 'Inerisci altri elementi',
   'fabric.elements.share.form.user-picker.validation.required':
     'Seleziona almeno un utente, un team o un’e-mail.',
-  'fabric.elements.share.form.user-picker.no-options.emptyQuery':
-    'Non è stato possibile trovare alcun risultato. Invita le persone utilizzando un indirizzo e-mail.',
-  'fabric.elements.share.form.user-picker.no-options':
-    'Non è stato possibile trovare risultati per "{inputValue}". Invita le persone utilizzando un indirizzo e-mail.',
+  'fabric.elements.share.form.user-picker.no-options.domainBasedUserOnly':
+    'Puoi invitare solo le persone con indirizzi e-mail {domains}. Contatta l’amministratore del sito per invitare altre persone.',
+  'fabric.elements.share.form.user-picker.no-options.existingUserOnly':
+    'Nessun risultato trovato per “{inputValue}”.',
+  'fabric.elements.share.form.user-picker.no-options.generic':
+    'Nessun risultato trovato per “{inputValue}”. Invita le persone inserendo il loro indirizzo e-mail.',
+  'fabric.elements.share.trigger.button.icon.label': 'Icona Condividi',
   'fabric.elements.share.trigger.button.text': 'Condividi',
+  'fabric.elements.share.trigger.button.invite.text': 'Invita',
+  'fabric.elements.share.copylink.button.icon.label':
+    'Icona Copia collegamento',
   'fabric.elements.share.copylink.button.text': 'Copia link',
+  'fabric.elements.share.copied.to.clipboard.icon.label':
+    'Icona Copia collegamento riuscita',
   'fabric.elements.share.copied.to.clipboard.message':
     'Link copiato negli appunti',
   'fabric.elements.share.form.capabilities.info.message':
     'Il tuo invito verrà mandato a un amministratore per l’approvazione',
+  'fabric.elements.share.failure.icon.label': 'Icona Errore',
   'fabric.elements.share.failure.message':
     'Impossibile effettuare la condivisione',
+  'fabric.elements.share.success.message':
+    '{object, select, blogpost {Blog post shared} board {Board shared} calendario {Calendar shared} bozza {Draft shared} filtro {Filter shared} ticket {Issue shared} file multimediali {Media shared} pagina {Page shared} progetto {Project shared} richiesta pull {Pull Request shared} domanda {Question shared} report {Report shared} repository {Repository shared} richiesta {Request shared} roadmap {Roadmap shared} sito {Site shared} spazio {Space shared} altro {Link Shared}}',
+  'fabric.elements.share.admin.notified.message':
+    'Il tuo amministratore è stato avvisato',
 };
