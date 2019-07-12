87f013562b7f270fce1aafc1b1df307a5d6c632e
Nathan Hur <nhur@atlassian.com>
2019-07-12 03:19:19 +0000

    DAC-4625 Update ADF url for DAC viewer storybook

--- packages/editor/renderer/examples/7-dac-viewer.tsx
+++ packages/editor/renderer/examples/7-dac-viewer.tsx
@@ -88,7 +88,7 @@ export default class Example extends PureComponent<{}, State> {
     this.setState({ fetchingSchema: true });
 
     fetch(
-      'https://unpkg.com/@atlaskit/editor-common@latest/dist/json-schema/v1/full.json',
+      'https://unpkg.com/@atlaskit/adf-schema@2.12.0/dist/json-schema/v1/full.json',
     )
       .then(res => {
         return res.json();
