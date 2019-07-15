d160c7b294af992db652babf2166e9a370a6c08f
Abhi Kishore <akishore@atlassian.com>
2019-07-15 14:32:31 +1000

    feat: pass prop to disable auth flow for mobile.

--- packages/media/smart-card/src/state/context/types.ts
+++ packages/media/smart-card/src/state/context/types.ts
@@ -2,10 +2,16 @@ import { CardStore, CardConnections } from '../store/types';
 import CardClient from '../../client';
 import { Store } from 'redux';
 
+// TODO: Remove once mobile team move to using authentication
+// flow https://product-fabric.atlassian.net/browse/SL-347.
+export interface CardAuthFlowOpts {
+  authFlow?: 'oauth2' | 'disabled';
+}
+
 export interface CardContext {
   store: Store<CardStore>;
   connections: CardConnections;
-  config: CardProviderCacheOpts;
+  config: CardProviderCacheOpts & CardAuthFlowOpts;
 }
 
 export interface CardProviderCacheOpts {
@@ -17,9 +23,9 @@ export interface CardProviderStoreOpts {
   initialState: CardStore;
 }
 
-export interface CardProviderProps {
+export type CardProviderProps = {
   client?: CardClient;
   cacheOptions?: CardProviderCacheOpts;
   storeOptions?: CardProviderStoreOpts;
   children: React.ReactNode;
-}
+} & CardAuthFlowOpts;
