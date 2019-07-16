f331aa7e64a68246d6f90b4887b43fc28d985cba
gwyneplaine <cc.lee@live.com.au>
2019-07-16 14:24:45 +1000

    separate out checkbox.tsx file into smaller component files in an elements folder

--- packages/core/checkbox/src/elements/IconWrapper.tsx
+++ packages/core/checkbox/src/elements/IconWrapper.tsx
@@ -1,21 +1,121 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { themed } from '@atlaskit/theme/components';
-import { gridSize } from '@atlaskit/theme/constants';
-import * as colors from '@atlaskit/theme/colors';
-import { multiply } from '@atlaskit/theme/math';
 import { ThemeTokens, ThemeIconTokens } from '../types';
 import React from 'react';
 
 interface Props {
   isActive?: boolean;
-  isChecked?: boolean;
+  isChecked?: boolean | unknown;
   isDisabled?: boolean;
   isFocused?: boolean;
   isInvalid?: boolean;
   isHovered?: boolean;
   rest?: any;
   tokens: ThemeTokens;
+}
+
+const disabledBorder = (iconTokens: ThemeIconTokens) => ({
+  stroke: iconTokens.borderColor.disabled,
+  strokeWidth: iconTokens.borderWidth,
+});
+
+const activeBorder = (iconTokens: ThemeIconTokens) => ({
+  stroke: iconTokens.borderColor.active,
+  strokeWidth: iconTokens.borderWidth,
+});
+
+const checkedBorder = (iconTokens: ThemeIconTokens) => ({
+  stroke: iconTokens.borderColor.checked,
+  strokeWidth: iconTokens.borderWidth,
+});
+
+const focusBorder = (iconTokens: ThemeIconTokens) => ({
+  stroke: iconTokens.borderColor.focused,
+  strokeWidth: iconTokens.borderWidth,
+});
+
+const invalidBorder = (iconTokens: ThemeIconTokens) => ({
+  stroke: iconTokens.borderColor.invalid,
+  strokeWidth: iconTokens.borderWidth,
+});
+
+const border = ({ isHovered, tokens: { icon } }: Props) => ({
+  stroke: isHovered ? icon.borderColor.hovered : icon.borderColor.rest,
+  strokeWidth: icon.borderWidth,
+});
+
+const getBorderColor = ({ tokens, ...props }: Props) => {
+  if (props.isDisabled) {
+    return disabledBorder(tokens.icon);
+  }
+  if (props.isActive) {
+    return activeBorder(tokens.icon);
+  }
+  if (props.isChecked) {
+    return checkedBorder(tokens.icon);
+  }
+  if (props.isFocused) {
+    return focusBorder(tokens.icon);
+  }
+  if (props.isInvalid) {
+    return invalidBorder(tokens.icon);
+  }
+  return border({ tokens, ...props });
+};
+
+const getTickColor = (props: Props) => {
+  const {
+    isChecked,
+    isDisabled,
+    isActive,
+    tokens: { icon },
+  } = props;
+
+  let color = icon.tickColor.checked;
+
+  if (isDisabled && isChecked) {
+    color = icon.tickColor.disabledAndChecked;
+  } else if (isActive && isChecked && !isDisabled) {
+    color = icon.tickColor.activeAndChecked;
+  } else if (!isChecked) {
+    color = icon.tickColor.rest;
+  }
+  return color;
+};
+
+const getBoxColor = (props: Props) => {
+  const {
+    isChecked,
+    isDisabled,
+    isActive,
+    isHovered,
+    tokens: { icon },
+  } = props;
+  // set the default
+  let color = icon.boxColor.rest;
+
+  if (isDisabled) {
+    color = icon.boxColor.disabled;
+  } else if (isActive) {
+    color = icon.boxColor.active;
+  } else if (isHovered && isChecked) {
+    color = icon.boxColor.hoveredAndChecked;
+  } else if (isHovered) {
+    color = icon.boxColor.hovered;
+  } else if (isChecked) {
+    color = icon.boxColor.checked;
+  }
+  return color;
+};
+
+export interface IconProps extends React.HTMLProps<HTMLLabelElement> {
+  tokens: ThemeTokens;
+  isChecked?: boolean;
+  isDisabled?: boolean;
+  isActive?: boolean;
+  isHovered?: boolean;
+  isFocused?: boolean;
+  isInvalid?: boolean;
 }
 
 export default ({ children, ...props }: IconProps) => (
