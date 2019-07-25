013e1d5eb5c4ba742791081130eaa26e7151baf5
Daniel Del Core <daniel.delcore.92@gmail.com>
2019-07-25 15:48:33 +1000

    updates entrypoints to provide fallbacks main entrypoints

--- build/multi-entry-tools/typescript-paths-tsconfig-builder.js
+++ build/multi-entry-tools/typescript-paths-tsconfig-builder.js
@@ -1,5 +1,7 @@
 const getAlternativeEntryPointAliasMap = require('./module-resolve-map-builder');
 const fromEntries = require('./utils/fromEntries');
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
   const isProjectLevel = process.argv[2] === '--project';
@@ -10,25 +12,20 @@ async function main() {
     Object.entries(mapping)
       .filter(([, path]) => path.includes('/packages/'))
       .map(([moduleName, modulePath]) => {
-        const modulePathWithoutExtension = modulePath.replace(
-          /(\.tsx?|\.js)$/,
-          '',
-        );
+        const modulePattern = moduleName.replace('/index', '');
+        const resolutionPath = modulePath
+          .replace('/index', '/')
+          .replace(/(\.tsx?|\.js)$/, '');
 
         return [
-          [moduleName],
+          [modulePattern],
           [
-            modulePathWithoutExtension.replace(`${cwd}/packages`, '../..'),
-            modulePathWithoutExtension.replace(`${cwd}/`, './'),
+            resolutionPath.replace(`${cwd}/packages`, '../..'),
+            resolutionPath.replace(`${cwd}/`, './'),
           ],
         ];
       }),
   );
-
-  paths['@atlaskit/analytics-next'] = [
-    'node_modules/@atlaskit/analytics-next-types',
-    'packages/elements/analytics-next-types',
-  ];
 
   console.log(
     '/* This file is auto-generated to get multi entry points to type check correctly */',
