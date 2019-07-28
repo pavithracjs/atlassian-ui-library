a20f51dbd3b5d948ba6a2704bb7f319af504ed2a
Jimmy Kurniawan <jkurniawan@atlassian.com>
2019-07-29 08:01:59 +1000

    FIND-131 Initial work to make cloud id optionals in atlassian switcher

---
+++ packages/navigation/atlassian-switcher/src/providers/with-handle-optional-cloud-id.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import {
+  DataProviderProps,
+  ProviderResult,
+  ResultComplete,
+  Status,
+} from './as-data-provider';
+
+type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U;
+
+export interface OptionalCloudId {
+  cloudId?: string;
+}
+
+/**
+ * Inject the ability to handle cases when cloudID is missing into the provided component.
+ *
+ * When cloud ID is available, this HOC will wrap {children} with the given provided component.
+ * Otherwise, {children} is executed with the provided fallback result.
+ *
+ * @param ProviderComponent component to wrap
+ * @param fallbackProviderResult result used to execute children if cloud id is missing
+ *
+ * @type P component props
+ * @type U provider result type
+ */
+function withHandleOptionalCloudId<U, P extends DataProviderProps<U>>(
+  ProviderComponent: React.ComponentType<P>,
+  fallbackProviderResult: U,
+) {
+  return function(props: Overwrite<P, OptionalCloudId>) {
+    const { cloudId } = props;
+    const children = (props.children as unknown) as ((
+      props: ProviderResult<U>,
+    ) => React.ReactNode);
+
+    if (cloudId) {
+      return <ProviderComponent {...props as P}>{children}</ProviderComponent>;
+    } else {
+      const resultComplete: ResultComplete<U> = {
+        status: Status.COMPLETE,
+        data: fallbackProviderResult,
+      };
+      return <React.Fragment>{children(resultComplete)}</React.Fragment>;
+    }
+  };
+}
+
+export default withHandleOptionalCloudId;
