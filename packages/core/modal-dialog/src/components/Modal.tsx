2712534a7e3fdddefed0b69e41d86592f08fb300
gwyneplaine <cc.lee@live.com.au>
2019-07-29 16:32:02 +1000

    fix type in modal-dialog to conform with ts 3.3.3

--- packages/core/modal-dialog/src/components/Modal.tsx
+++ packages/core/modal-dialog/src/components/Modal.tsx
@@ -31,12 +31,15 @@ interface PositionerProps {
   widthName?: WidthNames;
   widthValue?: string | number;
 }
-export function Positioner({ scrollBehavior, ...props }: PositionerProps) {
+export const Positioner: React.StatelessComponent<PositionerProps> = ({
+  scrollBehavior,
+  ...props
+}: PositionerProps) => {
   const PositionComponent =
     scrollBehavior === 'inside' ? PositionerAbsolute : PositionerRelative;
 
   return <PositionComponent {...props} />;
-}
+};
 
 function getScrollDistance() {
   return (
