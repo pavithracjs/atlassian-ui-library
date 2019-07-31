2b7dea96fa4aa68046679c7abbe0ea380b0d0abe
Juliano Prado <jprado@atlassian.com>
2019-07-31 15:08:08 +1000

    TEAMS-605 Adding analytics

--- packages/elements/mention/src/__tests__/unit/components/MentionSpotlightSpec.tsx
+++ packages/elements/mention/src/__tests__/unit/components/MentionSpotlightSpec.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import { mountWithIntl } from '@atlaskit/editor-test-helpers';
 import { noop } from '@babel/types';
 import Button from '@atlaskit/button';
-
 import MentionSpotlight, { Props } from '../../../components/MentionSpotlight';
-
+import * as SpotlightAnalytics from '../../../util/analytics';
 function render(props: Partial<Props>) {
   return mountWithIntl(
     <MentionSpotlight
@@ -17,17 +16,19 @@ function render(props: Partial<Props>) {
 
 let mockRegisterRender = jest.fn();
 let mockRegisterCreateLinkClick = jest.fn();
+let mockFireAnalyticsSpotlightMentionEvent = jest.fn();
+//mockFireAnalyticsSpotlightMentionEvent.mockReturnValue = () => jest.fn();
 
-jest.mock(
-  '../../../components/MentionSpotlight/MentionSpotlightController',
-  () => ({
-    __esModule: true,
-    default: {
-      registerRender: () => mockRegisterRender(),
-      registerCreateLinkClick: () => mockRegisterCreateLinkClick(),
-    },
-  }),
-);
+jest.mock('../../../util/analytics', () => {
+  const mockActualAnalytics = require.requireActual('../../../util/analytics');
+
+  return {
+    Actions: mockActualAnalytics.Actions,
+    ComponentNames: mockActualAnalytics.ComponentNames,
+    fireAnalyticsSpotlightMentionEvent: () =>
+      mockFireAnalyticsSpotlightMentionEvent,
+  };
+});
 
 describe('MentionSpotlight', () => {
   it('Should call onCall callback when the x is clicked', () => {
@@ -61,5 +62,18 @@ describe('MentionSpotlight', () => {
     expect(spotlight.html()).not.toBeNull();
     spotlight.find(Button).simulate('click');
     expect(spotlight.html()).toBeNull();
+  });
+
+  it('should send analytics data if the spotlight has been closed by the user', () => {
+    const onClose = jest.fn();
+    const spotlight = render({ onClose: onClose });
+    expect(spotlight.html()).not.toBeNull();
+    spotlight.find(Button).simulate('click');
+    expect(mockFireAnalyticsSpotlightMentionEvent).toHaveBeenCalledWith(
+      SpotlightAnalytics.ComponentNames.SPOTLIGHT,
+      SpotlightAnalytics.Actions.CLOSED,
+      SpotlightAnalytics.ComponentNames.MENTION,
+      'closeButton',
+    );
   });
 });
