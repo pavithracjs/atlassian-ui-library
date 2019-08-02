b2dfd51e5d6508d8848475564b1ac58189568cb6
raja <rbellebon@atlassian.com>
2019-08-02 10:46:03 +1000

    Add angular

--- packages/core/button/index.ts
+++ packages/core/button/index.ts
@@ -2,5 +2,8 @@ export * from './src';
 export { default } from './src';
 
 import * as jquery from 'jquery';
+import * as angular from 'angular';
 // eslint-disable-next-line no-console
 console.log(jquery);
+// eslint-disable-next-line no-console
+console.log(angular);
