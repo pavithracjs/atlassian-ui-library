5ad58dc8e22eb39f1507bc3545438933542b0ab8
Andrew Campbell <acampbell3@atlassian.com>
2019-08-02 17:08:28 +1000

    Add global type declaration to snapshot test

--- packages/core/comment/src/components/__tests__/visual-regression/commentSnapshotTest.ts
+++ packages/core/comment/src/components/__tests__/visual-regression/commentSnapshotTest.ts
@@ -3,6 +3,8 @@ import {
   takeScreenShot,
 } from '@atlaskit/visual-regression/helper';
 
+declare var global: any;
+
 describe('Snapshot Test', () => {
   it('Comment example should match production example', async () => {
     const url = getExampleUrl(
