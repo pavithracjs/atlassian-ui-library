e94990312a860a622d901d83d38deb3e276814bb
raja <rbellebon@atlassian.com>
2019-08-05 13:13:06 +1000

    Fix button scripts

--- packages/core/button/index.ts
+++ packages/core/button/index.ts
@@ -1,9 +1,2 @@
 export * from './src';
 export { default } from './src';
-
-import * as jquery from 'jquery';
-import * as angular from 'angular';
-// eslint-disable-next-line no-console
-console.log(jquery);
-// eslint-disable-next-line no-console
-console.log(angular);
