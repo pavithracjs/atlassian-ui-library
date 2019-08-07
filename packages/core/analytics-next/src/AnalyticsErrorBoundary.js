be7d77f186941e91b193e9a22039fadabe0d70a1
Will Mendes <willmendesneto@gmail.com>
2019-08-07 20:54:05 +1000

    feat(analytics-next): adding Analytics Error Boundary component

---
+++ packages/core/analytics-next/src/AnalyticsErrorBoundary.js
@@ -0,0 +1,116 @@
+// @flow
+import React, { type Node, Component } from 'react';
+import withAnalyticsEvents, {
+  type WithAnalyticsEventsProps,
+} from './withAnalyticsEvents';
+import AnalyticsContext from './AnalyticsContext';
+
+type AnalyticsErrorBoundaryErrorInfo = {
+  componentStack: string,
+};
+
+export type AnalyticsErrorBoundaryProps = WithAnalyticsEventsProps & {
+  /** React component to be wrapped */
+  children: Node,
+  /** Package name of the wrapped component (it will be added on payload) */
+  packageName: string,
+  /** Version of the wrapped component (it will be added on payload) */
+  componentVersion: string,
+  /** Name of the wrapped component (it will be added on payload) */
+  componentName: string,
+  /** The channel to listen for events on. */
+  channel?: string,
+};
+
+export type AnalyticsErrorBoundaryState = {
+  error: ?Error,
+  info: ?AnalyticsErrorBoundaryErrorInfo,
+};
+
+type AnalyticsErrorBoundaryPayload = {
+  error: ?Error | string,
+  info?: ?AnalyticsErrorBoundaryErrorInfo,
+  [key: string]: any,
+};
+
+const isObject = (o: any) =>
+  typeof o === 'object' && o !== null && !Array.isArray(o);
+
+export class BaseAnalyticsErrorBoundary extends Component<
+  AnalyticsErrorBoundaryProps,
+  AnalyticsErrorBoundaryState,
+> {
+  state = {
+    error: null,
+    info: null,
+  };
+
+  fireAnalytics = (analyticsErrorPayload: AnalyticsErrorBoundaryPayload) => {
+    const {
+      createAnalyticsEvent,
+      packageName,
+      componentVersion,
+      componentName,
+      channel,
+    } = this.props;
+
+    if (typeof createAnalyticsEvent === 'function') {
+      // Create our analytics event
+      const analyticsEvent = createAnalyticsEvent({
+        action: 'UnhandledError',
+        eventType: 'ui',
+        attributes: {
+          browserInfo:
+            window && window.navigator && window.navigator.userAgent
+              ? window.navigator.userAgent
+              : 'unknown',
+          componentName,
+          componentVersion,
+          packageName,
+          ...(isObject(analyticsErrorPayload) ? analyticsErrorPayload : {}),
+        },
+      });
+      // Fire our analytics event on the 'atlaskit' channel
+      analyticsEvent.fire(channel);
+    }
+  };
+
+  componentDidCatch(
+    error: Error,
+    info?: AnalyticsErrorBoundaryErrorInfo,
+  ): void {
+    const { createAnalyticsEvent } = this.props;
+    if (typeof createAnalyticsEvent === 'function') {
+      const payload: AnalyticsErrorBoundaryPayload = {
+        error,
+        info,
+      };
+      // Fire our analytics event on the given channel
+      this.fireAnalytics(payload);
+    }
+
+    this.setState({ error, info: info || null });
+  }
+
+  render() {
+    const { packageName, componentVersion, componentName } = this.props;
+
+    const analyticsContextData = {
+      packageName,
+      componentVersion,
+      componentName,
+    };
+
+    return (
+      <AnalyticsContext data={analyticsContextData}>
+        {this.props.children}
+      </AnalyticsContext>
+    );
+  }
+}
+
+const AnalyticsErrorBoundary = withAnalyticsEvents<AnalyticsErrorBoundaryProps>()(
+  BaseAnalyticsErrorBoundary,
+);
+
+export default AnalyticsErrorBoundary;
---
+++ packages/core/analytics-next/src/__tests__/unit/AnalyticsErrorBoundary.js
@@ -0,0 +1,75 @@
+// @flow
+
+import React from 'react';
+import { mount } from 'enzyme';
+import { BaseAnalyticsErrorBoundary as AnalyticsErrorBoundary } from '../../AnalyticsErrorBoundary';
+
+const props = {
+  channel: 'atlaskit',
+  componentName: 'button',
+  packageName: '@atlaskit/button',
+  componentVersion: '999.9.9',
+};
+
+describe('AnalyticsErrorBoundary', () => {
+  beforeEach(() => {
+    jest.spyOn(global.console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render the child component', () => {
+    const wrapper = mount(
+      <AnalyticsErrorBoundary {...props}>
+        <div className="child-component" />
+      </AnalyticsErrorBoundary>,
+    );
+
+    expect(wrapper.find('.child-component')).toHaveLength(1);
+  });
+
+  it('should fire an analytics event if error has been triggered in one of the children components', () => {
+    const error = new Error('Error');
+    const Something = (p: { error: boolean }) => {
+      if (p.error) {
+        throw error;
+      }
+      // this is just a placeholder
+      return <div className="child-component" />;
+    };
+
+    const fire = jest.fn();
+    const createAnalyticsEvent = jest.fn().mockImplementation(() => ({ fire }));
+    expect(() => {
+      mount(
+        <AnalyticsErrorBoundary
+          {...props}
+          createAnalyticsEvent={createAnalyticsEvent}
+        >
+          <Something error />
+        </AnalyticsErrorBoundary>,
+      );
+    }).toThrow();
+
+    expect(createAnalyticsEvent).toHaveBeenNthCalledWith(
+      1,
+      expect.objectContaining({
+        action: 'UnhandledError',
+        attributes: expect.objectContaining({
+          browserInfo: expect.any(String),
+          componentName: 'button',
+          componentVersion: '999.9.9',
+          error,
+          info: expect.objectContaining({
+            componentStack: expect.any(String),
+          }),
+          packageName: '@atlaskit/button',
+        }),
+        eventType: 'ui',
+      }),
+    );
+    expect(fire).toHaveBeenNthCalledWith(1, 'atlaskit');
+  });
+});
