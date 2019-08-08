b738f4f661ef726da833546fdb2740230ead04f5
Eduard Shvedai <eshvedai@atlassian.com>
2019-08-08 11:08:19 +1000

    ED-7344: trigger mutation observer callback only when new DIV nodes are inserted into table cells

--- packages/editor/editor-core/src/plugins/table/nodeviews/table.tsx
+++ packages/editor/editor-core/src/plugins/table/nodeviews/table.tsx
@@ -167,7 +167,7 @@ export default class TableView extends ReactNodeView {
     }
   };
 
-  private resizeForExtensionContent = (target: HTMLTableElement) => {
+  private resizeForExtensionContent = (target: HTMLElement) => {
     if (!this.node) {
       return;
     }
@@ -209,7 +209,14 @@ export default class TableView extends ReactNodeView {
 
     const uniqueTargets: Set<HTMLElement> = new Set();
     records.forEach(record => {
-      const target = record.target as HTMLTableElement;
+      const target = record.target as HTMLElement;
+      // ED-7344: ignore mutations that happen inside anything other than DIV or SPAN elements
+      if (
+        ['DIV', 'SPAN'].indexOf(target.tagName) === -1 ||
+        target.classList.contains(ClassName.RESIZE_HANDLE)
+      ) {
+        return;
+      }
       // If we've seen this target already in this set of targets
       // We dont need to reprocess.
       if (!uniqueTargets.has(target)) {
