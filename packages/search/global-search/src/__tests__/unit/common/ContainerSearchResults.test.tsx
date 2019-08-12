1cc5f523597ca2c4df98b8881137ae7706bc061b
Matthew Dyer <mdyer@atlassian.com>
2019-08-12 16:20:07 +1000

    QS-888: Remove typo quotes

--- packages/search/global-search/src/__tests__/unit/common/ContainerSearchResults.test.tsx
+++ packages/search/global-search/src/__tests__/unit/common/ContainerSearchResults.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { FormattedHTMLMessage } from 'react-intl';
 import uuid from 'uuid/v4';
-import { QuickSearchContext } from '../../../api/types';``
+import { QuickSearchContext } from '../../../api/types';
 import {
   BaseConfluenceQuickSearchContainer,
   BaseJiraQuickSearchContainerJira,
