494c3c5a881d8307e081501444a36f407495c5a9
Alex Reardon <areardon@atlassian.com>
2019-08-14 09:11:26 +1000

    fixing linting

--- packages/core/textfield/docs/0-intro.tsx
+++ packages/core/textfield/docs/0-intro.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { md, Example, code, Props } from '@atlaskit/docs';
-import { AkCode } from '@atlaskit/code';
-
-const attributes =
-  '[`React.InputHTMLAttributes<HTMLInputElement>`](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts#L1939)';
 
 export default md`
 A \`<input>\` component that supports elements at the start and end of the \`<input>\`
@@ -18,7 +14,6 @@ will be passed through to the underlying \`<input>\` element.
 
 For now, there are a few props that we apply directly based on the *public api* of this component.
 If you pass through a [\`React.InputHTMLAttributes<HTMLInputElement>\`](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts#L1939) prop that clashes with one of these, it will be overwritten and a warning will be logged.
-
 
   ## Examples
 
