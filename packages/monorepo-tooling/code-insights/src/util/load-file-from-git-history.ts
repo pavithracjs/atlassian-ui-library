202f7ed839bf956ead7a62f92db7b95e97f7159f
souravshaw1991 <sshaw2@atlassian.com>
2019-08-15 09:26:04 +0530

    changes execsync to exec as per Marco`s feedback

--- packages/monorepo-tooling/code-insights/src/util/load-file-from-git-history.ts
+++ packages/monorepo-tooling/code-insights/src/util/load-file-from-git-history.ts
@@ -1,4 +1,42 @@
-import { exec, execSync } from 'child_process';
+import { exec } from 'child_process';
+
+function getCurrentBranch(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(`git rev-parse --abbrev-ref HEAD`, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      }
+
+      if (stderr) {
+        reject(stderr);
+      }
+
+      resolve(stdout.toString());
+    });
+  });
+}
+
+function getancestorCommit(
+  branchName: string,
+  currentBranch: string,
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(
+      `git merge-base ${branchName} ${currentBranch}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        }
+
+        if (stderr) {
+          reject(stderr);
+        }
+
+        resolve(stdout.toString().replace(/\n$/, ''));
+      },
+    );
+  });
+}
 
 // Lock files tend to be huge
 const FiveMBBuffer = 1024 * 5000;
@@ -7,15 +45,9 @@ export default function loadFileFromGitHistory(
   branchName: string,
   fileName: string,
 ): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const currentBranch = execSync(
-      `git rev-parse --abbrev-ref HEAD`,
-    ).toString();
-    const ancestorCommit = execSync(
-      `git merge-base ${branchName} ${currentBranch}`,
-    )
-      .toString()
-      .replace(/(\r\n|\n|\r)/gm, '');
+  return new Promise(async (resolve, reject) => {
+    const currentBranch = await getCurrentBranch();
+    const ancestorCommit = await getancestorCommit(branchName, currentBranch);
     exec(
       `git show ${ancestorCommit}:${fileName}`,
       { maxBuffer: FiveMBBuffer },
