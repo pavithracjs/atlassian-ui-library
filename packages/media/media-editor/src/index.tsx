00a4b9c8039990a20f38fb404d008140e941a537
Will Mendes <willmendesneto@gmail.com>
2019-08-15 22:56:14 +1000

    chore(media-editor): removing example

--- packages/media/media-editor/src/index.tsx
+++ packages/media/media-editor/src/index.tsx
@@ -1,4 +1,3 @@
 export { default as SmartMediaEditor } from './react/smartMediaEditorLoader';
 export { default as EditorView } from './react/editorView/editorViewLoader';
-export { Dimensions, Tool } from './common';
-export { Blanket } from './react/styled';
+export { Dimensions } from './common';
