c4b723865fdd83bfccce77844ced827170c78fd0
Will Mendes <willmendesneto@gmail.com>
2019-08-15 19:34:14 +1000

    test(media-card): adding test for async import

--- packages/media/media-card/src/root/__tests__/unit/cardLoader.spec.tsx
+++ packages/media/media-card/src/root/__tests__/unit/cardLoader.spec.tsx
@@ -1,24 +1,81 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import { CardLoading } from '../../..';
-import CardLoader from '../../card/cardLoader';
+import { mount } from 'enzyme';
+import { fakeMediaClient, nextTick } from '@atlaskit/media-test-helpers';
+import { FileIdentifier } from '@atlaskit/media-client';
+import { CardLoading } from '../../../utils/lightCards/cardLoading';
+import CardLoader, {
+  CardWithMediaClientConfigProps,
+  AsyncCardState,
+} from '../../card/cardLoader';
 
-describe('CardLoader', () => {
-  it('should pass dimensions to the loading component', () => {
-    const dimensions = {
-      width: 10,
-      height: 10,
-    };
-    const mediaClientConfig = {} as any;
-    const identifier = {} as any;
-    const component = shallow(
-      <CardLoader
-        mediaClientConfig={mediaClientConfig}
-        identifier={identifier}
-        dimensions={dimensions}
-      />,
-    );
+const mediaClient = fakeMediaClient();
 
-    expect(component.find(CardLoading).prop('dimensions')).toEqual(dimensions);
+const identifier: FileIdentifier = {
+  id: 123,
+  mediaItemType: 'file',
+  collectionName: 'some-name',
+};
+
+const props = {
+  dimensions: {
+    width: 10,
+    height: 10,
+  },
+  mediaClientConfig: mediaClient.config,
+  identifier,
+};
+
+describe('Async Card Loader', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('When the async import returns with error', () => {
+    beforeEach(() => {
+      jest.mock('../../card', () => {
+        throw new Error('Forcing async import error');
+      });
+    });
+
+    it('should pass dimensions to the loading component if the async components were NOT resolved', async () => {
+      const wrapper = mount(<CardLoader {...props} />);
+
+      await nextTick();
+
+      expect(wrapper.find(CardLoading).prop('dimensions')).toEqual(
+        props.dimensions,
+      );
+    });
+
+    it('should NOT render Card component', async () => {
+      const wrapper = mount<CardWithMediaClientConfigProps, AsyncCardState>(
+        <CardLoader {...props} />,
+      );
+
+      await nextTick();
+
+      expect(wrapper.find(CardLoading).prop('dimensions')).toEqual(
+        props.dimensions,
+      );
+
+      expect(wrapper.state().Card).toBeUndefined();
+    });
+  });
+
+  describe('When the async import returns with success', () => {
+    beforeEach(() => {
+      jest.unmock('../../card');
+    });
+
+    it('should render Card component', async () => {
+      const wrapper = await mount<
+        CardWithMediaClientConfigProps,
+        AsyncCardState
+      >(<CardLoader {...props} />);
+
+      await nextTick();
+
+      expect(wrapper.state().Card).not.toBeUndefined();
+    });
   });
 });
