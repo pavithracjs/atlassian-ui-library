7b794e7cea401e7e34fbeedeb2b75df92a332f94
Samuel Lewis <slewis@atlassian.com>
2019-08-19 10:29:32 +1000

    ED-2774 Fix pasting of superscript and subscript from google docs by adding unique paste handler

--- packages/editor/adf-schema/src/schema/marks/subsup.ts
+++ packages/editor/adf-schema/src/schema/marks/subsup.ts
@@ -17,6 +17,16 @@ export interface SubSupMark extends Mark {
   attrs: SubSupAttributes;
 }
 
+function getAttrFromVerticalAlign(node: HTMLElement) {
+  if (node.style.verticalAlign) {
+    const type = node.style.verticalAlign.slice(0, 3);
+    if (type === 'sub' || type === 'sup') {
+      return { type };
+    }
+  }
+  return false;
+}
+
 export const subsup: MarkSpec = {
   inclusive: true,
   group: FONT_STYLE,
@@ -24,6 +34,18 @@ export const subsup: MarkSpec = {
   parseDOM: [
     { tag: 'sub', attrs: { type: 'sub' } },
     { tag: 'sup', attrs: { type: 'sup' } },
+    {
+      // Special case for pasting from Google Docs
+      // Google Docs uses vertical align to denote subscript and super script
+      tag: 'span',
+      style: 'vertical-align=super',
+      getAttrs: node => getAttrFromVerticalAlign(node as HTMLElement),
+    },
+    {
+      tag: 'span',
+      style: 'vertical-align=sub',
+      getAttrs: node => getAttrFromVerticalAlign(node as HTMLElement),
+    },
   ],
   toDOM(mark) {
     return [mark.attrs.type];
