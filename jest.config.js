375a7f22484157debc59e39282dd54745ff4cbb2
Gora Kong <gora.kong1@gmail.com>
2019-08-21 17:58:46 -0700

    fix tsconfig & baseurl

--- jest.config.js
+++ jest.config.js
@@ -71,7 +71,7 @@ const config = {
   },
   globals: {
     'ts-jest': {
-      tsConfigFile: './tsconfig.jest.json',
+      tsConfig: './tsconfig.jest.json',
     },
     __BASEURL__: 'http://localhost:9000',
   },
