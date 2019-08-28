7fc471c764f5894c0467ceaca2c667d3257b3211
Tung Dang <tthanhdang@atlassian.com>
2019-08-28 17:33:31 +1000

    PTC-2466: ProfileCardResourced should trigger analytics event when having error

---
+++ packages/people-and-teams/profilecard/src/internal/analytics.ts
@@ -0,0 +1,8 @@
+export const AnalyticsName = {
+  PROFILE_CARD_CLICK: 'profile-card.click',
+  PROFILE_CARD_VIEW: 'profile-card.view',
+  PROFILE_CARD_LOADED: 'profile-card.loaded',
+  PROFILE_CARD_RELOAD: 'profile-card.reload',
+  PROFILE_CARD_ERROR: 'profile-card.error',
+  PROFILE_CARD_RESOURCE_ERROR: 'profile-card-resource.error',
+};
