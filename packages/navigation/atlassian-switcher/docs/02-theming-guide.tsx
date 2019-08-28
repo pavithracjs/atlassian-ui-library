d3e2b59fe723ce41d99259a02cfae4b3f55ace7c
Leandro Lemos <llemos@atlassian.com>
2019-08-28 17:20:05 +1000

    FIND-133: Adding docs

--- packages/navigation/atlassian-switcher/docs/02-theming-guide.tsx
+++ packages/navigation/atlassian-switcher/docs/02-theming-guide.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { md, code, Example } from '@atlaskit/docs';
+import details from './switcher-theming.png';
 
 export default md`
   The switcher supports a limited set of customizations through the atlaskit theming api. The following properties are exposed:
@@ -8,41 +9,62 @@ export default md`
   - primaryHoverBackgroundColor: background color used on hover state for top level and child items
   - secondaryTextColor: background color used on passive hover state for top level and default background for child items
 
+  ### Spec:
+  ${<img src={details} />}
+
+  ### Usage:
   ${code`
-// MyComponent.js
-import React from 'react';
-import Switcher, { createCustomTheme } from '@atlaskit/atlassian-switcher"';
+  // MyComponent.js
+  import React from 'react';
+  import Switcher, { createCustomTheme } from '@atlaskit/atlassian-switcher"';
 
-export default (props) => {
-  const myTheme = {
-    primaryTextColor: colors.text,
-    secondaryTextColor: '#03396c',
-    primaryHoverBackgroundColor: '#ccffff',
-    secondaryHoverBackgroundColor: '#e5ffff',
-  };
+  export default (props) => {
+    const myTheme = {
+      primaryTextColor: colors.text,
+      secondaryTextColor: '#03396c',
+      primaryHoverBackgroundColor: '#ccffff',
+      secondaryHoverBackgroundColor: '#e5ffff',
+    };
 
-  const customTheme = createCustomTheme(myTheme);
+    const customTheme = createCustomTheme(myTheme);
 
-  return (
-    <Switcher
-      {...props}
-      theme={customTheme}
-      }}
-    />
-  );
-}
-`}
+    return (
+      <Switcher
+        {...props}
+        theme={customTheme}
+        }}
+      />
+    );
+  }
+  `}
 
-  Check it an example below:
+  ### Examples:
 
   ${(
     <Example
       packageName="@atlaskit/atlassian-switcher"
-      Component={
-        require('../examples/02-inline-dialog-theming-example').default
-      }
-      title="Rendered on an inline dialog with theming"
-      source={require('!!raw-loader!../examples/02-inline-dialog-theming-example')}
+      Component={require('../examples/01-standalone').default}
+      title="Standalone switcher"
+      source={require('!!raw-loader!../examples/01-standalone')}
     />
   )}
+
+  ${(
+    <Example
+      packageName="@atlaskit/atlassian-switcher"
+      Component={require('../examples/01-standalone-themed-green').default}
+      title="Standalone switcher with green theme"
+      source={require('!!raw-loader!../examples/01-standalone-themed-green')}
+    />
+  )}
+
+  ${(
+    <Example
+      packageName="@atlaskit/atlassian-switcher"
+      Component={require('../examples/01-standalone-themed-red').default}
+      title="Standalone switcher with red theme"
+      source={require('!!raw-loader!../examples/01-standalone-themed-red')}
+    />
+  )}
+
 `;
