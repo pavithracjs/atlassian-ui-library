8362b3eba303269bdb13a2d49d787f775cd933be
Leandro Lemos <llemos@atlassian.com>
2019-08-30 15:49:15 +1000

    FIND-190: Removing obsolete tests

--- packages/navigation/atlassian-switcher/src/__tests__/unit/atlassian-switcher.tsx
+++ packages/navigation/atlassian-switcher/src/__tests__/unit/atlassian-switcher.tsx
@@ -1,43 +1,18 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import AtlassianSwitcher from '../../index';
-import JiraSwitcher from '../../components/jira-switcher';
-import ConfluenceSwitcher from '../../components/confluence-switcher';
-import GenericSwitcher from '../../components/generic-switcher';
 
 const noop = () => void 0;
 
-const renderSpecificTest = (
-  testTitle: string,
-  product: string,
-  Ctor: React.ComponentType<any>,
-) => {
-  it(testTitle, () => {
+describe('Atlassian Switcher', () => {
+  it('basic snapshot', () => {
     const switcher = shallow(
       <AtlassianSwitcher
-        product={product}
+        product="jira"
         cloudId="CLOUD_ID"
         triggerXFlow={noop}
       />,
     );
     expect(switcher).toMatchSnapshot();
   });
-};
-
-describe('Atlassian Switcher', () => {
-  renderSpecificTest(
-    'should render correct switcher for `jira`',
-    'jira',
-    JiraSwitcher,
-  );
-  renderSpecificTest(
-    'should render correct switcher for `confluence`',
-    'confluence',
-    ConfluenceSwitcher,
-  );
-  renderSpecificTest(
-    'should render correct switcher for `site-admin`',
-    'site-admin',
-    GenericSwitcher,
-  );
 });
